d52fea045838bd61616ec640e17b493a0b68fa8c
Ravishankar <rchandh@ncsu.edu>
2016-07-28 12:46:59 -0400

    Create backup copy for temporary json data files.
    The backup copy will be cleared once in 15 minutes.

--- daemonset/getmetrics_daemonset.py
+++ daemonset/getmetrics_daemonset.py
@@ -234,6 +234,7 @@ def update_docker():
     dockers = os.listdir("/var/lib/docker/containers")
     cronfile = open(os.path.join(homepath,datadir+"getmetrics_docker.sh"),'w')
     cronfile.write("#!/bin/sh\nDATADIR='data/'\ncd $DATADIR\n")
+    cronfile.write("now=$(date +%M)\n")
     containerCount = 0
     for container in dockers:
         if container == "":
@@ -243,6 +244,19 @@ def update_docker():
         cronfile.write(command+"\n")
     for i in range(1,containerCount+1):
         cronfile.write("wait $PID"+str(i)+"\n")
+    cronfile.write("if [ $now -eq \"00\" ] || [ $now -eq \"15\" ] || [ $now -eq \"30\" ] || [ $now -eq \"45\" ];\nthen\n")
+    for container in dockers:
+        if container == "":
+            continue
+        command = "    cat stat"+container+".txt > stat"+container+"_backup.txt\n"
+        cronfile.write(command)
+    cronfile.write("else\n")
+    for container in dockers:
+        if container == "":
+            continue
+        command = "    cat stat"+container+".txt >> stat"+container+"_backup.txt\n"
+        cronfile.write(command)
+    cronfile.write("fi\n")
     cronfile.close()
     os.chmod(os.path.join(homepath,datadir+"getmetrics_docker.sh"),0755)
 
