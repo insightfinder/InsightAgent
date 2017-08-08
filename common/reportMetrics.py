7de4d3c8ede4fe5d8839b2d1b18286666db39124
Moharnab Saikia <moharnab123saikia@gmail.com>
2017-08-08 17:15:05 -0400

    change default reporting path

--- common/reportMetrics.py
+++ common/reportMetrics.py
@@ -19,8 +19,8 @@ if prev endtime is 0, report most recent reporting interval
 till now from today's log file (may or may not be present)
 assumping gmt epoch timestamp and local date daily file
 '''
-#serverUrl = 'https://agent-data.insightfinder.com'
-serverUrl = 'http://localhost:8080'
+serverUrl = 'https://agent-data.insightfinder.com'
+#serverUrl = 'http://localhost:8080'
 usage = "Usage: %prog [options]"
 parser = OptionParser(usage=usage)
 parser.add_option("-f", "--fileInput",
