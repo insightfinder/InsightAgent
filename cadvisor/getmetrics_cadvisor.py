3ad3247137370607bf46c09dda7594a7e8b57a6d
Moharnab Saikia <moharnab123saikia@gmail.com>
2017-10-06 16:23:16 -0400

    refactor cadvisor - remove global code

--- cadvisor/getmetrics_cadvisor.py
+++ cadvisor/getmetrics_cadvisor.py
@@ -9,85 +9,54 @@ import socket
 from optparse import OptionParser
 import json
 
-usage = "Usage: %prog [options]"
-parser = OptionParser(usage=usage)
-parser.add_option("-d", "--directory",
-                  action="store", dest="homepath", help="Directory to run from")
-(options, args) = parser.parse_args()
 
-if options.homepath is None:
-    homepath = os.getcwd()
-else:
-    homepath = options.homepath
-datadir = 'data/'
-
-hostname = socket.gethostname()
-cAdvisoraddress = "http://" + hostname + ":8080/api/v1.3/docker/"
-
-command = ['bash', '-c', 'source ' + str(homepath) + '/.agent.bashrc && env']
-proc = subprocess.Popen(command, stdout=subprocess.PIPE)
-for line in proc.stdout:
-    (key, _, value) = line.partition("=")
-    os.environ[key] = value.strip()
-proc.communicate()
-samplingInterval = os.environ["SAMPLING_INTERVAL"]
-if samplingInterval[-1:] == 's':
-    samplingInterval = int(samplingInterval[:-1])
-else:
-    samplingInterval = int(samplingInterval) * 60 / 2
-
-counter_time_map = {}
-counter = 0
-##the default value it 60-1, when cAdvisor started, the code need to calculate the index because of the sliding window
-index = 59
-
-def getindex(colName):
-    if colName == "CPU":
+def get_index(column_name):
+    if column_name == "CPU":
         return 3001
-    elif colName == "DiskRead" or colName == "DiskWrite":
+    elif column_name == "DiskRead" or column_name == "DiskWrite":
         return 3002
-    elif colName == "NetworkIn" or colName == "NetworkOut":
+    elif column_name == "NetworkIn" or column_name == "NetworkOut":
         return 3003
-    elif colName == "MemUsed":
+    elif column_name == "MemUsed":
         return 3004
 
 
-def update_container_count(cadvisor_json, date):
-    dockerInstances = []
-    newInstances = []
-    if os.path.isfile(os.path.join(homepath, datadir + "totalInstances.json")) == False:
-        towritePreviousInstances = {}
-        for containers in cadvisor_json:
-            if containers != "":
-                dockerInstances.append(containers)
-        towritePreviousInstances["overallDockerInstances"] = dockerInstances
+def update_container_count(cadvisor_json, date, datadir):
+    docker_instances = []
+    new_instances = []
+    previous_instances = {}
+    if not os.path.isfile(os.path.join(homepath, datadir + "totalInstances.json")):
+        for container in cadvisor_json:
+            if container != "":
+                docker_instances.append(container)
+        previous_instances["overallDockerInstances"] = docker_instances
         with open(os.path.join(homepath, datadir + "totalInstances.json"), 'w') as f:
-            json.dump(towritePreviousInstances, f)
+            json.dump(previous_instances, f)
     else:
         with open(os.path.join(homepath, datadir + "totalInstances.json"), 'r') as f:
-            dockerInstances = json.load(f)["overallDockerInstances"]
-        for containers in cadvisor_json:
-            if containers != "":
-                newInstances.append(containers)
-        if cmp(newInstances,dockerInstances) != 0:
-            towritePreviousInstances = {}
-            towritePreviousInstances["overallDockerInstances"] = newInstances
+            docker_instances = json.load(f)["overallDockerInstances"]
+        for container in cadvisor_json:
+            if container != "":
+                new_instances.append(container)
+        if cmp(new_instances, docker_instances) != 0:
+            previous_instances["overallDockerInstances"] = new_instances
             with open(os.path.join(homepath, datadir + "totalInstances.json"), 'w') as f:
-                json.dump(towritePreviousInstances, f)
-            if os.path.isfile(os.path.join(homepath, datadir + date + ".csv")) == True:
+                json.dump(previous_instances, f)
+            if os.path.isfile(os.path.join(homepath, datadir + date + ".csv")):
                 oldFile = os.path.join(homepath, datadir + date + ".csv")
                 newFile = os.path.join(homepath, datadir + date + "." + time.strftime("%Y%m%d%H%M%S") + ".csv")
                 os.rename(oldFile, newFile)
 
 
-def getmetric():
+def get_metric():
     global counter_time_map
     global counter
     global index
-    global cAdvisoraddress
-    global dockerInstances
-    global samplingInterval
 
+    datadir = 'data/'
+    hostname = socket.gethostname()
+    cAdvisoraddress = "http://" + hostname + ":8000/api/v1.3/docker/"
+    sampling_interval = get_project_settings()
     try:
         startTime = int(round(time.time() * 1000))
         date = time.strftime("%Y%m%d")
@@ -100,32 +69,34 @@ def getmetric():
                     print "unable to get requests from ", cAdvisoraddress
                     sys.exit()
                 continue
-            update_container_count(r.json(), date)
+
+            update_container_count(r.json(), date, datadir)
+
             try:
                 for key, value in r.json().items():
                     index = len(r.json()[key]["stats"]) - 1
                     break;
-            except KeyError, ve:
+            except KeyError:
                 print "Unable to get stats information from cAdvisor"
                 sys.exit()
+
             time_stamp = startTime
             if (time_stamp in counter_time_map.values()):
                 continue
             counter_time_map[counter] = time_stamp
             counter = (counter + 1) % 60
             log = str(startTime)
-            cpu_all = 0
             resource_usage_file = open(os.path.join(homepath, datadir + date + ".csv"), 'a+')
             numlines = len(resource_usage_file.readlines())
             i = 0
             for key, value in r.json().items():
                 index = len(r.json()[key]["stats"]) - 1
-                if index - int(samplingInterval) < 0:
-                    samplingInterval = 1
+                if index - int(sampling_interval) < 0:
+                    sampling_interval = 1
                 try:
                     cpu_used = r.json()[key]["stats"][index]["cpu"]["usage"][
                         "total"]
-                    prev_cpu = r.json()[key]["stats"][index - int(samplingInterval)]["cpu"][
+                    prev_cpu = r.json()[key]["stats"][index - int(sampling_interval)]["cpu"][
                         "usage"]["total"]
                     cur_cpu = float(float(cpu_used - prev_cpu) / 1000000000)
                     cur_cpu = abs(cur_cpu)
@@ -146,23 +117,23 @@ def getmetric():
                     curr_block_num = len(r.json()[key]["stats"][index]["diskio"]["io_service_bytes"])
                     curr_io_read = 0
                     curr_io_write = 0
-                    prev_io_read = 0
-                    prev_io_write = 0
                     for j in range(curr_block_num):
                         curr_io_read += r.json()[key]["stats"][index]["diskio"]["io_service_bytes"][j]["stats"]["Read"]
                         curr_io_write += r.json()[key]["stats"][index]["diskio"]["io_service_bytes"][j]["stats"][
                             "Write"]
                     prev_block_num = len(
-                        r.json()[key]["stats"][index - int(samplingInterval)]["diskio"]["io_service_bytes"])
+                        r.json()[key]["stats"][index - int(sampling_interval)]["diskio"]["io_service_bytes"])
                     prev_io_read = 0
                     prev_io_write = 0
                     for j in range(prev_block_num):
                         prev_io_read += \
-                        r.json()[key]["stats"][index - int(samplingInterval)]["diskio"]["io_service_bytes"][j]["stats"][
-                            "Read"]
+                            r.json()[key]["stats"][index - int(sampling_interval)]["diskio"]["io_service_bytes"][j][
+                                "stats"][
+                                "Read"]
                         prev_io_write += \
-                        r.json()[key]["stats"][index - int(samplingInterval)]["diskio"]["io_service_bytes"][j]["stats"][
-                            "Write"]
+                            r.json()[key]["stats"][index - int(sampling_interval)]["diskio"]["io_service_bytes"][j][
+                                "stats"][
+                                "Write"]
                     io_read = float(float(curr_io_read - prev_io_read) / (1024 * 1024))  # MB
                     io_write = float(float(curr_io_write - prev_io_write) / (1024 * 1024))  # MB
                 except KeyError, e:
@@ -171,8 +142,8 @@ def getmetric():
                     io_write = "NaN"
                 # get network
                 try:
-                    prev_network_t = r.json()[key]["stats"][index - int(samplingInterval)]["network"]["tx_bytes"]
-                    prev_network_r = r.json()[key]["stats"][index - int(samplingInterval)]["network"]["rx_bytes"]
+                    prev_network_t = r.json()[key]["stats"][index - int(sampling_interval)]["network"]["tx_bytes"]
+                    prev_network_r = r.json()[key]["stats"][index - int(sampling_interval)]["network"]["rx_bytes"]
                     curr_network_t = r.json()[key]["stats"][index]["network"]["tx_bytes"]
                     curr_network_r = r.json()[key]["stats"][index]["network"]["rx_bytes"]
                     network_t = float(float(curr_network_t - prev_network_t) / (1024 * 1024))  # MB
@@ -193,7 +164,7 @@ def getmetric():
                             continue
                         if (fieldnames != ""):
                             fieldnames = fieldnames + ","
-                        groupid = getindex(fields[k])
+                        groupid = get_index(fields[k])
 
                         dockerID = r.json()[key]["aliases"][0]
                         docker_id_split = []
@@ -204,7 +175,10 @@ def getmetric():
                             if c == '.':
                                 docker_id_split = dockerID.rsplit('.', 1)
                                 break
-                        if len(docker_id_split) != 0:
+                        if len(docker_id_split) == 2 and len(docker_id_split[0]) > len(docker_id_split[1]):
+                            # if c == '_' and not docker_id_split[1].isdigit():
+                            #     dockerID = r.json()[key]["aliases"][0]
+                            # else:
                             dockerID = docker_id_split[0]
                         metric = fields[k] + "[" + dockerID + "_" + host + "]"
                         fieldnames = fieldnames + metric + ":" + str(groupid)
@@ -212,8 +186,8 @@ def getmetric():
             if (numlines < 1):
                 resource_usage_file.write("%s\n" % (fieldnames))
             print "Log: " + str(log)  # is it possible that print too many things?
-            writelog = log
-            resource_usage_file.write("%s\n" % (writelog))
+            # writelog = log
+            resource_usage_file.write("%s\n" % (log))
             resource_usage_file.flush()
             resource_usage_file.close()
             break;
@@ -221,12 +195,43 @@ def getmetric():
         print "Keyboard Interrupt"
 
 
-def main():
+def parse_options():
+    global homepath
+    usage = "Usage: %prog [options]"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-d", "--directory",
+                      action="store", dest="homepath", help="Directory to run from")
+    (options, args) = parser.parse_args()
+    if options.homepath is None:
+        homepath = os.getcwd()
+    else:
+        homepath = options.homepath
+
+
+def get_project_settings():
+    command = ['bash', '-c', 'source ' + str(homepath) + '/.agent.bashrc && env']
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE)
+    for line in proc.stdout:
+        (key, _, value) = line.partition("=")
+        os.environ[key] = value.strip()
+    proc.communicate()
+    samplingInterval = os.environ["SAMPLING_INTERVAL"]
+    if samplingInterval[-1:] == 's':
+        samplingInterval = int(samplingInterval[:-1])
+    else:
+        samplingInterval = int(samplingInterval) * 60 / 2
+    return samplingInterval
+
+
+if __name__ == "__main__":
+    parse_options()
+    get_project_settings()
+    counter_time_map = {}
+    counter = 0
+    ##the default value it 60-1, when cAdvisor started, the code need to calculate the index because of the sliding window
+    index = 59
     try:
-        getmetric()
+        get_metric()
     except (KeyboardInterrupt, SystemExit):
         print "Keyboard Interrupt"
         sys.exit()
-
-if __name__ == "__main__":
-    main()
