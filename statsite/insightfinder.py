08e1835cfc936b2adbba0dd4790c9c2fe4844a07
Moharnab Saikia <moharnab123saikia@gmail.com>
2018-05-10 14:31:18 -0400

    chunking enabled

--- statsite/insightfinder.py
+++ statsite/insightfinder.py
@@ -14,8 +14,36 @@ import json
 import time
 import os
 
-# Initialize the logger
-logging.basicConfig()
+##
+# InsightFinder sink for statsite
+# ==========================
+#
+# Use with the following stream command:
+#
+#  stream_command = python sinks/insightfinder.py insightfinder.ini INFO 3
+#
+# The InsightFinder sink takes an INI format configuration file as a first
+# argument , log level as a second argument and no. of re-connect attempts as the third argument.
+# The following is an example configuration:
+#
+# Configuration example:
+# ---------------------
+#
+# [insightfinder]
+# username = user
+# project_name = statsite
+# license_key = c123439662ad1edf64e99e97e4b776112345678
+# sampling_interval = 10
+# url = https://app.insightfinder.com
+#
+# Options:
+# --------
+#  - username:  Insightfinder application user-name. You can get it from this page: https://app.insightfinder.com/account-info
+#  - project_name: InsightFinder project to send data to
+#  - license_key: InsightFinder license key. You can get it from this page: https://app.insightfinder.com/account-info
+#  - sampling_interval: statsite sampling interval
+#  - url (optional) : Host url to send data to. Its https://app.insightfinder.com by default
+###
 
 SPACES = re.compile(r"\s+")
 SLASHES = re.compile(r"\/+")
@@ -24,161 +52,220 @@ NON_ALNUM = re.compile(r"[^a-zA-Z_\-0-9\.]")
 
 class InsightfinderStore(object):
 
-		def __init__(self, cfg="/home/moharnab/statsite/insightfinder.ini", prefix="statsite.", attempts=3):
+    def __init__(self, cfg="/etc/insightfinder.ini", lvl="INFO", attempts=3):
+        """
+        Implements an interface that allows metrics to be persisted to InsightFinder.
 
-			attempts = int(attempts)
-			self.logger = logging.getLogger("statsite.insightfinderstore")
-			self.prefix = prefix
-			self.attempts = attempts
-			self.metrics_map = {}
-			self.to_send_metrics = []
-			self.cfg = cfg
-			self.load(cfg)
+        Raises a :class:`ValueError` on bad arguments or `Exception` on missing
+        configuration section.
 
-		def load(self, cfg):
-			# Loads configuration from an INI format file
-			import ConfigParser
-			ini = ConfigParser.RawConfigParser()
-			ini.read(cfg)
+        :Parameters:
+                - `cfg` (optional) : INI configuration file.
+                - `lvl` (optional) : logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+                - `attempts` (optional) : The number of re-connect retries before failing.
+		"""
+        if attempts < 1:
+            raise ValueError("Must have at least 1 attempt!")
 
-			sect = "insightfinder"
-			if not ini.has_section(sect):
-				raise Exception("Can not locate config section '" + sect + "'")
+        attempts = int(attempts)
+        self.logger = logging.getLogger("statsite.insightfinderstore")
+        self.logger.setLevel(lvl)
+        self.attempts = attempts
+        self.metrics_map = {}
+        self.to_send_metrics = []
+        self.cfg = cfg
+        self.load(cfg)
+        self.temp_group_id = 10000
 
-			if ini.has_option(sect, 'username'):
-				self.username = ini.get(sect, 'username')
-			else:
-				raise ValueError("username must be set in config")
+    def load(self, cfg):
+        """Loads configuration from an INI format file"""
+        import ConfigParser
+        ini = ConfigParser.RawConfigParser()
+        ini.read(cfg)
 
-			if ini.has_option(sect, 'project_name'):
-				self.project_name = ini.get(sect, 'project_name')
-			else:
-				raise ValueError("project_name must be set in config")
+        sect = "insightfinder"
+        if not ini.has_section(sect):
+            raise Exception("Can not locate config section '" + sect + "'")
 
-			if ini.has_option(sect, 'license_key'):
-				self.license_key = ini.get(sect, 'license_key')
-			else:
-				raise ValueError("license_key must be set in config")
+        if ini.has_option(sect, 'username'):
+            self.username = ini.get(sect, 'username')
+        else:
+            raise ValueError("username must be set in config")
 
-			self.url = 'https://app.insightfinder.com'
-			if ini.has_option(sect, 'url'):
-				self.url = ini.get(sect, 'url')
+        if ini.has_option(sect, 'project_name'):
+            self.project_name = ini.get(sect, 'project_name')
+        else:
+            raise ValueError("project_name must be set in config")
 
-		def get_grouping_id(self, metricName, tempID):
-			
-			if(os.path.isfile('grouping.json')):
-				self.logger.info("Grouping file exists. Continuing..");
-			else:
-				self.logger.info("Grouping file doesn't exist. Creating new file.");
-				grouping_file = open('grouping.json', "w")
-				grouping_file.write('{}')
-				grouping_file.close()
-			grouping_id = 0
-			with open('grouping.json', 'r+') as f:
-				grouping_obj = json.loads(f.read())
-				if metricName in grouping_obj.keys():
-					grouping_id = int(grouping_obj[metricName])
-				else:
-					grouping_id = tempID
-				grouping_obj[metricName] = grouping_id
-				f.seek(0)
-				f.write(json.dumps(grouping_obj))
-				f.truncate()
-			return grouping_id				
+        if ini.has_option(sect, 'license_key'):
+            self.license_key = ini.get(sect, 'license_key')
+        else:
+            raise ValueError("license_key must be set in config")
 
-		def flush(self, metrics):
-			"""
-			Flushes the metrics provided to InfluxDB.
+        self.url = 'https://app.insightfinder.com'
+        if ini.has_option(sect, 'url'):
+            self.url = ini.get(sect, 'url')
+        self.sampling_interval = 10
+        if ini.has_option(sect, 'sampling_interval'):
+            self.sampling_interval = int(ini.get(sect, 'sampling_interval'))
 
-			Parameters:
-			- `metrics` : A list of (key,value,timestamp) tuples.
-			"""
-			if not metrics:
-				return
-			hostname = socket.gethostname()
-			temp_group_id = 10000
-			for metric in metrics:
-				if len(metric) != 0:
-					metric_split = metric.split("|")
-					metric_key = metric_split[0]
-					metric_value = metric_split[1]
-					timestamp = int(metric_split[2])
+    def get_grouping_id(self, metric_key, temp_id):
+        """
+        Get grouping id for a metric key 
 
-					if timestamp in self.metrics_map:
-						value_map = self.metrics_map[timestamp]
-					else:
-						value_map = {}
+        Parameters:
+        - `metric_key` : metric key str to get group id.
+        - `temp_id` : proposed group id integer
+        """
+        if(os.path.isfile('grouping.json')):
+            self.logger.debug("Grouping file exists. Continuing..")
+        else:
+            self.logger.debug(
+                "Grouping file doesn't exist. Creating new file.")
+            grouping_file = open('grouping.json', "w")
+            grouping_file.write('{}')
+            grouping_file.close()
+        grouping_id = 0
+        with open('grouping.json', 'r+') as f:
+            grouping_obj = json.loads(f.read())
+            if metric_key in grouping_obj.keys():
+                grouping_id = int(grouping_obj[metric_key])
+            else:
+                grouping_id = temp_id
+            grouping_obj[metric_key] = grouping_id
+            f.seek(0)
+            f.write(json.dumps(grouping_obj))
+            f.truncate()
+        return grouping_id
 
-					value_map[metric_key + '[' + hostname + ']:' + str(self.get_grouping_id(metric_key, temp_group_id))] = metric_value
-					temp_group_id += 1
-					print value_map
-					self.metrics_map[timestamp] = value_map
-			print self.metrics_map
-			for timestamp in self.metrics_map.keys():
-				value_map = self.metrics_map[timestamp]
-				value_map['timestamp'] = str(timestamp) + '000'
-				self.to_send_metrics.append(value_map)
+    def normalize_key(self, key):
+        """
+		Take a single key string and return the same string with spaces, slashes and
+		non-alphanumeric characters subbed out and prefixed by self.prefix.
+        """
+        key = SPACES.sub("_", key)
+        key = SLASHES.sub("-", key)
+        key = NON_ALNUM.sub("", key)
+    	# key = "%s%s" % (self.prefix, key)
+        return key
 
-			print self.to_send_metrics
+    def append(self, metric):
+        """
+        Flushes the metrics provided to InsightFinder.
 
-			self.send_data(self.to_send_metrics)
+        Parameters:
+        - `metric` : A string entry with format key|value|timestamp.
+        """
+        if not metric and metric.count("|") != 2:
+            return
+        hostname = socket.gethostname()
 
-		def send_data(self, metric_data):
-			
-			if not metric_data or len(metric_data) == 0:
-				return
-			
-			send_data_time = time.time()
-			# prepare data for metric streaming agent
-			to_send_data_dict = {}
-			to_send_data_dict["metricData"] = json.dumps(metric_data)
-			to_send_data_dict["licenseKey"] = self.license_key
-			to_send_data_dict["projectName"] = self.project_name
-			to_send_data_dict["userName"] = self.username
-			to_send_data_dict["instanceName"] = socket.gethostname().partition(".")[0]
-			to_send_data_dict["samplingInterval"] = str(int(10))
-			to_send_data_dict["agentType"] = "custom"
+        if len(metric) != 0:
+            metric_split = metric.split("|")
+            metric_key = self.normalize_key(metric_split[0])
+            metric_value = metric_split[1]
+            timestamp = int(metric_split[2])
 
-			to_send_data_json = json.dumps(to_send_data_dict)
-			print "to send"
-			print to_send_data_json
-			self.logger.info("TotalData: " + str(len(bytearray(to_send_data_json))))
+            if timestamp in self.metrics_map:
+                value_map = self.metrics_map[timestamp]
+            else:
+                value_map = {}
 
-		    # send the data
-			postUrl = self.url + "/customprojectrawdata"
-			response = requests.post(postUrl, data=json.loads(to_send_data_json))
-			if response.status_code == 200:
-				print "successfully sent data"
-				self.logger.info(str(len(bytearray(to_send_data_json))) + " bytes of data are reported.")
-				# updateLastSentFiles(pcapFileList)
-			else:
-				self.logger.info("Failed to send data.")
-				print "failed to send data"
-			self.logger.debug("--- Send data time: %s seconds ---" % (time.time() - send_data_time))
+            value_map[metric_key + '[' + hostname + ']:' +
+                      str(self.get_grouping_id(metric_key, self.temp_group_id))] = metric_value
+            self.temp_group_id += 1
+            self.metrics_map[timestamp] = value_map
 
+    def send_metrics(self):
+        self.logger.info("Outputting %d metrics", sum(len(v)
+                                                      for v in self.metrics_map.itervalues()))
+        self._flush_lines()
+        self.metrics_map = {}
+        self.to_send_metrics = []
 
+    def _flush_lines(self):
+        """ Flushes the metrics provided to InsightFinder. """
+        if not self.metrics_map:
+            return
+        # reformat data as a list of JSON Objects for each timestamp
+        for timestamp in self.metrics_map.keys():
+            value_map = self.metrics_map[timestamp]
+            value_map['timestamp'] = str(timestamp) + '000'
+            self.to_send_metrics.append(value_map)
+
+        try:
+            self._write_metric(self.to_send_metrics)
+        except StandardError:
+            self.logger.exception("Failed to write out the metrics!")
+
+    def _write_metric(self, metric):
+        """Tries to create a JSON message and send to the InsightFinder URL, reconnecting on any errors"""
+        for _ in xrange(self.attempts):
+            try:
+                self._send_data(metric)
+                return
+            except IOError:
+                self.logger.exception(
+                    "Error while flushing to InsightFinder. Reattempting...")
+
+        self.logger.critical(
+            "Failed to flush to Insightfinder! Gave up after %d attempts.", self.attempts)
+
+    def _send_data(self, metric_data):
+
+        if not metric_data or len(metric_data) == 0:
+            self.logger.warning("No data to send for this flush.")
+            return
+
+        send_data_time = time.time()
+
+        # prepare data for metric streaming agent
+        to_send_data_dict = {}
+        to_send_data_dict["metricData"] = json.dumps(metric_data)
+        to_send_data_dict["licenseKey"] = self.license_key
+        to_send_data_dict["projectName"] = self.project_name
+        to_send_data_dict["userName"] = self.username
+        to_send_data_dict["instanceName"] = socket.gethostname().partition(".")[
+            0]
+        to_send_data_dict["samplingInterval"] = str(self.sampling_interval)
+        to_send_data_dict["agentType"] = "custom"
+
+        to_send_data_json = json.dumps(to_send_data_dict)
+        self.logger.debug(
+            "TotalData: " + str(len(bytearray(to_send_data_json))))
+
+    	# send the data
+        postUrl = self.url + "/customprojectrawdata"
+        response = requests.post(postUrl, data=json.loads(to_send_data_json))
+        if response.status_code == 200:
+            self.logger.info(str(len(bytearray(to_send_data_json))
+                                 ) + " bytes of data are reported.")
+        else:
+            self.logger.exception("Failed to send data.")
+            raise IOError("Failed to send request to " + postUrl)
+        self.logger.debug("--- Send data time: %s seconds ---" %
+                          (time.time() - send_data_time))
 
 
 def main():
-	# Intialize from our arguments
-	insightfinder = InsightfinderStore(*sys.argv[1:])
+    # Initialize the logger
+    logging.basicConfig()
 
-	METRICS_PER_FLUSH = 50
+    # Intialize from our arguments
+    insightfinder = InsightfinderStore(*sys.argv[1:])
 
-	lines = sys.stdin.read().split("\n")
-	insightfinder.flush(lines)
-	# Get all the inputs
-	# for line in lines:
-	# 	print "Output: " + str(line)
-	# 	if len(insightfinder.metrics) >= METRICS_PER_FLUSH:
-	# 		print "Output: " + str(line)
-            # insightfinder.send_metrics()
-        # insightfinder.append(line.strip())
+    METRICS_BYTES_PER_FLUSH = 3000000
 
-    # insightfinder.send_metrics()
-	# print "End"
+    # Get all the inputs
+    for line in sys.stdin:
+        map_size = len(bytearray(json.dumps(insightfinder.metrics_map)))
+        if map_size >= METRICS_BYTES_PER_FLUSH:
+            insightfinder.send_metrics()
+        insightfinder.append(line.strip())
+
+    insightfinder.send_metrics()
 
 
 if __name__ == "__main__":
-	main()
+    main()
 
