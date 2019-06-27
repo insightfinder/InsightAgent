ee35a9ff98ba6e77a1b6d9b87c125ad17667eabf
root <root@ip-172-31-51-204.ec2.internal>
2019-06-27 19:40:54 +0000

    add SASL support

--- kafka/getmetrics_kafka.py
+++ kafka/getmetrics_kafka.py
@@ -81,6 +81,7 @@ def get_agent_config_vars():
             group_id = parser.get('kafka', 'group_id')
             client_id = parser.get('kafka', 'client_id')
             data_send_timeout = parser.get('kafka', 'data_send_timeout')
+            # SSL
             security_protocol = parser.get('kafka', 'security_protocol')
             ssl_context = parser.get('kafka','ssl_context')
             ssl_check_hostname = parser.get('kafka','ssl_check_hostname')
@@ -89,7 +90,15 @@ def get_agent_config_vars():
             ssl_key = parser.get('kafka','ssl_key')
             ssl_password = parser.get('kafka','ssl_password')
             ssl_crl = parser.get('kafka','ssl_crl')
-            ssl_ciphers = parser.get('kafka','ssl_ciphers')>
+            ssl_ciphers = parser.get('kafka','ssl_ciphers')
+
+            # SASL
+            sasl_mechanism = parser.get('kafka', 'sasl_mechanism')
+            sasl_plain_username = parser.get('kafka', 'sasl_plain_username')
+            sasl_plain_password = parser.get('kafka', 'sasl_plain_password')
+            sasl_kerberos_service_name = parser.get('kafka', 'sasl_kerberos_service_name')
+            sasl_kerberos_domain_name = parser.get('kafka', 'sasl_kerberos_domain_name')
+            sasl_oauth_token_provider = parser.get('kafka', 'sasl_oauth_token_provider')
 
             if len(insightFinder_license_key) == 0:
                 logger.error("Agent not correctly configured(license key). Check config file.")
@@ -125,6 +134,8 @@ def get_agent_config_vars():
             config_vars['groupId'] = group_id
             config_vars['clientId'] = client_id
             config_vars['dataSendTimeout'] = data_send_timeout    
+
+            # SSL
             config_vars['security_protocol'] = security_protocol
             config_vars['ssl_context'] =  ssl_context
             config_vars['ssl_check_hostname'] =  ssl_check_hostname
@@ -135,6 +146,13 @@ def get_agent_config_vars():
             config_vars['ssl_crl'] =  ssl_crl
             config_vars['ssl_ciphers'] =  ssl_ciphers
  
+            #SASL
+            config_vars['sasl_mechanism'] = sasl_mechanism
+            config_vars['sasl_plain_username'] = sasl_plain_username
+            config_vars['sasl_plain_password'] = sasl_plain_password
+            config_vars['sasl_kerberos_service_name'] = sasl_kerberos_service_name
+            config_vars['sasl_kerberos_domain_name'] = sasl_kerberos_domain_name
+            config_vars['sasl_oauth_token_provider'] = sasl_oauth_token_provider
     except IOError:
         logger.error("config.ini file is missing")
     return config_vars
@@ -475,6 +493,15 @@ def kafka_data_consumer(consumer_id):
         kafka_kwargs['ssl_password'] = agent_config_vars['ssl_password'] if agent_config_vars['ssl_password'] != ""
         kafka_kwargs['ssl_crl'] = agent_config_vars['ssl_crl'] if agent_config_vars['ssl_crl'] != ""
         kafka_kwargs['ssl_ciphers'] = agent_config_vars['ssl_ciphers'] if agent_config_vars['ssl_ciphers'] != ""
+
+    # add SASL info
+    if len(agent_config_vars['sasl_mechanism']) != 0:
+        kafka_kwargs['sasl_mechanism'] = agent_config_vars['sasl_mechanism'] if agent_config_vars['sasl_mechanism'] != ""
+        kafka_kwargs['sasl_plain_username'] = agent_config_vars['sasl_plain_username'] if agent_config_vars['sasl_plain_username'] != ""
+        kafka_kwargs['sasl_plain_password'] = agent_config_vars['sasl_plain_password'] if agent_config_vars['sasl_plain_password'] != ""
+        kafka_kwargs['sasl_kerberos_service_name'] = agent_config_vars['sasl_kerberos_service_name'] if agent_config_vars['sasl_kerberos_service_name'] != ""
+        kafka_kwargs['sasl_kerberos_domain_name'] = agent_config_vars['sasl_kerberos_domain_name'] if agent_config_vars['sasl_kerberos_domain_name'] != ""
+        kafka_kwargs['sasl_oauth_token_provider '] = agent_config_vars['sasl_oauth_token_provider '] if agent_config_vars['sasl_oauth_token_provider '] != ""
 
     logger.debug(kafka_kwargs)
     consumer = KafkaConsumer(**kafka_kwargs)
