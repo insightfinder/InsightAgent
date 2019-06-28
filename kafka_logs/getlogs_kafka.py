dba57adbf54b5cd8dbe0b0117c4371418e134c40
root <root@ip-172-31-51-204.ec2.internal>
2019-06-28 18:25:18 +0000

    fix post-cond syntax

--- kafka_logs/getlogs_kafka.py
+++ kafka_logs/getlogs_kafka.py
@@ -331,23 +331,23 @@ def kafka_data_consumer(consumer_id):
     # add SSL info
     if agent_config_vars['security_protocol'] == 'SSL': 
         kafka_kwargs['security_protocol'] = 'SSL'
-        kafka_kwargs['ssl_context'] = agent_config_vars['ssl_context'] if agent_config_vars['ssl_context'] != ""
-        kafka_kwargs['ssl_check_hostname'] = agent_config_vars['ssl_check_hostname'] if agent_config_vars['ssl_check_hostname'] != ""
-        kafka_kwargs['ssl_ca'] = agent_config_vars['ssl_ca'] if agent_config_vars['ssl_ca'] != ""
-        kafka_kwargs['ssl_certificate'] = agent_config_vars['ssl_certificate'] if agent_config_vars['ssl_certificate'] != ""
-        kafka_kwargs['ssl_key'] = agent_config_vars['ssl_key'] if agent_config_vars['ssl_key'] != ""
-        kafka_kwargs['ssl_password'] = agent_config_vars['ssl_password'] if agent_config_vars['ssl_password'] != ""
-        kafka_kwargs['ssl_crl'] = agent_config_vars['ssl_crl'] if agent_config_vars['ssl_crl'] != ""
-        kafka_kwargs['ssl_ciphers'] = agent_config_vars['ssl_ciphers'] if agent_config_vars['ssl_ciphers'] != ""
+        kafka_kwargs['ssl_context'] = agent_config_vars['ssl_context'] if agent_config_vars['ssl_context'] != "" else None
+        kafka_kwargs['ssl_check_hostname'] = agent_config_vars['ssl_check_hostname'] if agent_config_vars['ssl_check_hostname'] != "" else None
+        kafka_kwargs['ssl_ca'] = agent_config_vars['ssl_ca'] if agent_config_vars['ssl_ca'] != "" else None
+        kafka_kwargs['ssl_certificate'] = agent_config_vars['ssl_certificate'] if agent_config_vars['ssl_certificate'] != "" else None
+        kafka_kwargs['ssl_key'] = agent_config_vars['ssl_key'] if agent_config_vars['ssl_key'] != "" else None
+        kafka_kwargs['ssl_password'] = agent_config_vars['ssl_password'] if agent_config_vars['ssl_password'] != "" else None
+        kafka_kwargs['ssl_crl'] = agent_config_vars['ssl_crl'] if agent_config_vars['ssl_crl'] != "" else None
+        kafka_kwargs['ssl_ciphers'] = agent_config_vars['ssl_ciphers'] if agent_config_vars['ssl_ciphers'] != "" else None
 
     # add SASL info
     if len(agent_config_vars['sasl_mechanism']) != 0:
-        kafka_kwargs['sasl_mechanism'] = agent_config_vars['sasl_mechanism'] if agent_config_vars['sasl_mechanism'] != ""
-        kafka_kwargs['sasl_plain_username'] = agent_config_vars['sasl_plain_username'] if agent_config_vars['sasl_plain_username'] != ""
-        kafka_kwargs['sasl_plain_password'] = agent_config_vars['sasl_plain_password'] if agent_config_vars['sasl_plain_password'] != ""
-        kafka_kwargs['sasl_kerberos_service_name'] = agent_config_vars['sasl_kerberos_service_name'] if agent_config_vars['sasl_kerberos_service_name'] != ""
-        kafka_kwargs['sasl_kerberos_domain_name'] = agent_config_vars['sasl_kerberos_domain_name'] if agent_config_vars['sasl_kerberos_domain_name'] != ""
-        kafka_kwargs['sasl_oauth_token_provider '] = agent_config_vars['sasl_oauth_token_provider '] if agent_config_vars['sasl_oauth_token_provider '] != ""
+        kafka_kwargs['sasl_mechanism'] = agent_config_vars['sasl_mechanism']
+        kafka_kwargs['sasl_plain_username'] = agent_config_vars['sasl_plain_username'] if agent_config_vars['sasl_plain_username'] != "" else None
+        kafka_kwargs['sasl_plain_password'] = agent_config_vars['sasl_plain_password'] if agent_config_vars['sasl_plain_password'] != "" else None
+        kafka_kwargs['sasl_kerberos_service_name'] = agent_config_vars['sasl_kerberos_service_name'] if agent_config_vars['sasl_kerberos_service_name'] != "" else None
+        kafka_kwargs['sasl_kerberos_domain_name'] = agent_config_vars['sasl_kerberos_domain_name'] if agent_config_vars['sasl_kerberos_domain_name'] != "" else None
+        kafka_kwargs['sasl_oauth_token_provider '] = agent_config_vars['sasl_oauth_token_provider '] if agent_config_vars['sasl_oauth_token_provider '] != "" else None
 
     logger.debug(kafka_kwargs)
     consumer = KafkaConsumer(**kafka_kwargs)
