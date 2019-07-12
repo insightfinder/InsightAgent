97258303589323ada0db404e6a30aededd710b5d
gnegri <grantnegri@gmail.com>
2019-07-12 15:46:36 -0400

    fix errors

--- kafka2/kafka2-config-monit.sh
+++ kafka2/kafka2-config-monit.sh
@@ -4,6 +4,8 @@ AGENT="kafka2"
 AGENT_SCRIPT="getmessages_kafka.py"
 AGENT_FULL_PATH="$(pwd)/${AGENT_SCRIPT}"
 AGENT_FULL_PATH_CONFIG="$(pwd)/config.ini"
+AGENT_FULL_PATH_LOG="$(pwd)/log.out"
+touch ${AGENT_FULL_PATH_LOG}
 
 # monit control
 MONIT_FILE="/etc/monit.d/${AGENT}.monit"
@@ -11,13 +13,13 @@ touch ${MONIT_FILE}
 echo \
 "check process kafka_agent matching \"${AGENT_FULL_PATH}\"
     if does not exist then start
-    start program = \"/usr/bin/nohup /usr/bin/python ${AGENT_FULL_PATH}\"
-    stop program = \"pkill -f ${AGENT_FULL_PATH}\"
+    start program = \"/usr/bin/python ${AGENT_FULL_PATH} &>${AGENT_FULL_PATH_LOG}\"
+    stop program = \"/usr/bin/pkill -f ${AGENT_FULL_PATH}\"
 
 check file kafka_agent_config path \"${AGENT_FULL_PATH_CONFIG}\"
     if changed timestamp then restart
-    start program = \"/usr/bin/nohup /usr/bin/python ${AGENT_FULL_PATH}\"
-    stop program = \"pkill -f ${AGENT_FULL_PATH}\"" > ${MONIT_FILE}
+    start program = \"/usr/bin/python ${AGENT_FULL_PATH} &>${AGENT_FULL_PATH_LOG}\"
+    stop program = \"/usr/bin/pkill -f ${AGENT_FULL_PATH}\"" > ${MONIT_FILE}
 
 # reload monit
 monit reload
