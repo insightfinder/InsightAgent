ba371e3927d8ff755064639e04e77c04e10aad27
gnegri <grantnegri@gmail.com>
2019-08-12 15:24:32 -0400

    fixes

--- mysql_log_collector/pip-mysql-log.sh
+++ mysql_log_collector/pip-mysql-log.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # hard-coded definition
+PIP_VERSION="pip-19.2.2.tar.gz"
 PIP_PACKAGES="mysql-connector"
 if [[ -z "${PIP_PACKAGES}" ]]; then
     # try to read from stdin
@@ -7,10 +8,10 @@ if [[ -z "${PIP_PACKAGES}" ]]; then
 fi
 
 # run as root
-if [[ $EUID -ne 0 ]]; then
-   echo "This script should be ran as root. Exiting..."
-   exit 1
-fi
+#if [[ $EUID -ne 0 ]]; then
+#   echo "This script should be ran as root. Exiting..."
+#   exit 1
+#fi
 
 # if pip packages are needed
 if [[ -n "${PIP_PACKAGES}" ]]; then
@@ -18,42 +19,20 @@ if [[ -n "${PIP_PACKAGES}" ]]; then
     # attempt online install
     if [[ -z $(command -v pip) ]]; then
         echo "Package \"pip\" not installed. Attempting to install now..."
-        python <(curl https://bootstrap.pypa.io/get-pip.py)
-    fi
-
-    # if still not found, try to find locally
-    if [[ -z $(command -v pip) ]]; then
-        find ./ -type f -name "get-pip.py" -exec python {} \;
-    fi
-
-    # if still not found, try to find somewhat locally
-    if [[ -z $(command -v pip) ]]; then
-        find ../../ -type f -name "get-pip.py" -exec python {} \;
-    fi
-
-    # if still not found, expand to filesystem
-    if [[ -z $(command -v pip) ]]; then
-        find / -type f -name "get-pip.py" -exec python {} \;
+        python <(curl --connect-timeout 3 https://bootstrap.pypa.io/get-pip.py)
     fi
 
     # if still not found, quit
     if [[ -z $(command -v pip) ]]; then
-        echo "Could not find \"get-pip.py\". Please download and install it."
-        echo "On a machine that is connected to the internet, run:\
-            curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py\
-        then copy get-pip.py to this machine and run\
-            python get-pip.py"
+        echo "Could not install package \"pip\". Please contact support@insightfinder.com"
         exit 1
     fi
 
     ####
     # similar to the above, but look for pip_packages.tar.gz
-    if [[ ! $(pip install ${PIP_PACKAGES}) ]]; then
+    if [[ ! $(pip install --no-index --find-links='pip_packages' ${PIP_PACKAGES}) ]]; then
         if [[ ! -d "pip_packages" ]]; then
-            find ./ -type f -name "pip_packages.tar.gz" -exec tar xf {} \;
-        fi
-        if [[ ! -d "pip_packages" ]]; then
-            find ../../ -type f -name "pip_packages.tar.gz" -exec tar xf {} \;
+            find ../ -type f -name "pip_packages.tar.gz" -exec tar xf {} \;
         fi
         if [[ ! -d "pip_packages" ]]; then
             find / -type f -name "pip_packages.tar.gz" -exec tar xf {} \;
