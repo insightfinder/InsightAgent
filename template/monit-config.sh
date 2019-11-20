e52a38109f312947470828447f01c54a7a428adb
gnegri <grantnegri@gmail.com>
2019-11-20 14:14:16 -0500

    update installer

--- template/monit-config.sh
+++ template/monit-config.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# TODO: fill out agent name
-AGENT=""
-AGENT_SCRIPT=".py"
-
 # run as root
 if [[ $EUID -ne 0 ]]; then
    echo "This script should be ran as root. Exiting..."
@@ -16,6 +12,20 @@ if [[ -z $(command -v monit) ]]; then
     exit 1
 fi
 
+# get agent
+AGENT=$(pwd | awk -F "/" '{print $NF}')
+#AGENT_SCRIPT=$(find . -type f -name "get[^\-]*.py" -print)
+AGENT_SCRIPT=$(\ls -l | grep get[^\-]*.py | awk '{print $NF}')
+if [[ -z ${AGENT_SCRIPT} ]];
+then
+    AGENT_SCRIPT=$(find . -type f -name "replay*.py" -print)
+fi
+if [[ -z ${AGENT_SCRIPT} ]];
+then
+    echo "No script found. Exiting..."
+    exit 1
+fi
+AGENT_SCRIPT=${AGENT_SCRIPT:2:${#AGENT_SCRIPT}}
 AGENT_FULL_PATH="$(pwd)/${AGENT_SCRIPT}"
 AGENT_FULL_PATH_CONFIG="$(pwd)/config.ini"
 AGENT_FULL_PATH_LOG="$(pwd)/log.out"
@@ -24,19 +34,21 @@ touch ${AGENT_FULL_PATH_LOG}
 # monit control
 MONIT_FILE="/etc/monit.d/${AGENT}"
 touch ${MONIT_FILE}
-echo \
-"check process ${AGENT} matching \"${AGENT_FULL_PATH}\"
+cat <<EOF > ${MONIT_FILE}
+check process ${AGENT} matching \"${AGENT_FULL_PATH}\"
     if does not exist then start
-    start program = \"\$(command -v python) ${AGENT_FULL_PATH} &>${AGENT_FULL_PATH_LOG}\"
+    start program = \"\$(command -v python) ${AGENT_FULL_PATH} >${AGENT_FULL_PATH_LOG}\"
     stop program = \"\$(command -v pkill) -f ${AGENT_FULL_PATH}\"
 
 check file ${AGENT}_config path \"${AGENT_FULL_PATH_CONFIG}\"
     if changed timestamp then restart
-    start program = \"\$(command -v python) ${AGENT_FULL_PATH} &>${AGENT_FULL_PATH_LOG}\"
-    stop program = \"\$(command -v pkill) -f ${AGENT_FULL_PATH}\"" > ${MONIT_FILE}
+    start program = \"\$(command -v python) ${AGENT_FULL_PATH} >${AGENT_FULL_PATH_LOG}\"
+    stop program = \"\$(command -v pkill) -f ${AGENT_FULL_PATH}\"
+EOF
 
 echo "monit config file created at ${MONIT_FILE}"
 cat ${MONIT_FILE}
 
 # reload monit
 monit reload
+exit 0
