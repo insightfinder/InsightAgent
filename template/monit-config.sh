aeb7fd8aacd8a92f7c6896b9dd41cf18a7459517
gnegri <grantnegri@gmail.com>
2019-11-20 16:45:01 -0500

    update monit

--- template/monit-config.sh
+++ template/monit-config.sh
@@ -12,43 +12,61 @@ if [[ -z $(command -v monit) ]]; then
     exit 1
 fi
 
+# get input params
+DRY_RUN=1
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -c|--create)
+            DRY_RUN=0
+            ;;
+    esac
+    shift
+done
+
 # get agent
 AGENT=$(pwd | awk -F "/" '{print $NF}')
-#AGENT_SCRIPT=$(find . -type f -name "get[^\-]*.py" -print)
-AGENT_SCRIPT=$(\ls -l | grep get[^\-]*.py | awk '{print $NF}')
+AGENT_SCRIPT=$(\ls -l  | awk '{print $NF}' | grep ^get[^\-].*\.py$)
 if [[ -z ${AGENT_SCRIPT} ]];
 then
-    AGENT_SCRIPT=$(find . -type f -name "replay*.py" -print)
+    AGENT_SCRIPT=$(\ls -l  | awk '{print $NF}' | grep ^replay.*\.py$)
 fi
 if [[ -z ${AGENT_SCRIPT} ]];
 then
     echo "No script found. Exiting..."
     exit 1
 fi
-AGENT_SCRIPT=${AGENT_SCRIPT:2:${#AGENT_SCRIPT}}
 AGENT_FULL_PATH="$(pwd)/${AGENT_SCRIPT}"
 AGENT_FULL_PATH_CONFIG="$(pwd)/config.ini"
 AGENT_FULL_PATH_LOG="$(pwd)/log.out"
-touch ${AGENT_FULL_PATH_LOG}
+MONIT_FILE="/etc/monit.d/${AGENT}"
+
+if [[ ${DRY_RUN} -eq 0 ]];
+then
+    touch ${AGENT_FULL_PATH_LOG}
+    touch ${MONIT_FILE}
+fi
 
 # monit control
-MONIT_FILE="/etc/monit.d/${AGENT}"
-touch ${MONIT_FILE}
-cat <<EOF > ${MONIT_FILE}
+echo "
 check process ${AGENT} matching \"${AGENT_FULL_PATH}\"
     if does not exist then start
-    start program = \"\$(command -v python) ${AGENT_FULL_PATH} >${AGENT_FULL_PATH_LOG}\"
-    stop program = \"\$(command -v pkill) -f ${AGENT_FULL_PATH}\"
+    start program = \"/bin/bash -c \'\$(command -v python) ${AGENT_FULL_PATH} &>${AGENT_FULL_PATH_LOG}\"'
+    stop program = \"/bin/bash -c \'\$(command -v pkill) -f ${AGENT_FULL_PATH}\"'
 
 check file ${AGENT}_config path \"${AGENT_FULL_PATH_CONFIG}\"
     if changed timestamp then restart
-    start program = \"\$(command -v python) ${AGENT_FULL_PATH} >${AGENT_FULL_PATH_LOG}\"
-    stop program = \"\$(command -v pkill) -f ${AGENT_FULL_PATH}\"
-EOF
+    start program = \"/bin/bash -c \'\$(command -v python) ${AGENT_FULL_PATH} &>${AGENT_FULL_PATH_LOG}\"'
+    stop program = \"/bin/bash -c \'\$(command -v pkill) -f ${AGENT_FULL_PATH}\"'
+" |& if [[ ${DRY_RUN} -eq 0 ]]; then tee ${MONIT_FILE}; else awk '{print}'; fi
+echo ""
 
-echo "monit config file created at ${MONIT_FILE}"
-cat ${MONIT_FILE}
-
-# reload monit
-monit reload
+if [[ ${DRY_RUN} -eq 0 ]];
+then
+    echo "Monit config file created at ${MONIT_FILE}"
+    cat ${MONIT_FILE}
+    monit reload
+else
+    echo "Rerun this script in commit mode to create the above monit config at ${MONIT_FILE}"
+    echo "./monit-config.sh --create"
+fi
 exit 0
