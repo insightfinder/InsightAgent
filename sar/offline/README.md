df74c12fa6143322af977ec3a4af6a1b47397eb8
gnegri <grantnegri@gmail.com>
2019-11-29 14:58:25 -0500

    add sysstat offline install

--- sar/offline/README.md
+++ sar/offline/README.md
@@ -1,8 +1,10 @@
 # Offline Install
-These scripts provide support for installing sar from source. To use them, simply run
-
+These scripts provide support for installing sar from source. To use them, simply run (from an internet-connected machine):
 ```bash
 ./prepare-git-repo.sh
+```
+
+```bash
 ./make-install.sh   # to install locally
                     # or, to install on remote machine(s):
 ./remote-cp-run.sh -t sysstat-make.tar.gz -s make-install.sh -p sysstat [-f nodefile list_of_nodes]
--- sar/offline/make-install.sh
+++ sar/offline/make-install.sh
@@ -7,19 +7,31 @@ then
 else
     TARGET="$1"
 fi
-
 if [[ -z ${TARGET} ]];
 then
     echo "No target specified (as first parameter)"
 fi
 
+# get tarball
 TARBALL="${TARGET}-make.tar.gz"
 TARBALL_LOC=$(find . -type f -name ${TARBALL} -print)
-if [[ ! -f ${TARBALL_LOC} ]];
+if [[ ! -f ${TARBALL_LOC} || -z ${TARBALL_LOC} ]];
 then
     TARBALL_LOC=$(find /tmp -type f -name ${TARBALL} -print)
 fi
+if [[ ! -f ${TARBALL_LOC} || -z ${TARBALL_LOC} ]];
+then
+    echo "No tarball could be found in $(pwd) or /tmp"
+else
+    tar xf ${TARBALL_LOC}
+fi
 
-tar xf ${TARBALL_LOC}
+# make sure target folder exists
+if [[ ! -d ${TARGET} ]];
+then
+    echo "Could not find directory ${TARGET}"
+fi
+
 cd ${TARGET}
+make
 make install
