c249fee0292d49b5701e794251c41219ab6bb898
gnegri <grantnegri@gmail.com>
2019-12-02 17:27:20 -0500

    support offline

--- sar/offline/fetch-prereqs.sh
+++ sar/offline/fetch-prereqs.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+ALWAYS_DOWNLOAD=0
+if [[ "$@" =~ -r|--remote ]];
+then
+    ALWAYS_DOWNLOAD=1
+    shift
+fi
+
 GNU_MIRROR="$1"
 if [[ -z ${GNU_MIRROR} ]];
 then
@@ -7,36 +14,13 @@ then
 else
     shift
 fi
-# really just for debugging
-ALWAYS_DOWNLOAD=1
 
 CURL="curl -sSL"
-
-function configure() {
-    TAR="$1"
-    shift
-    if [[ -n ${TAR} && -f ${TAR} ]];
-    then
-        DIR=$(tar tf ${TAR} | head -n1)
-        tar xf ${TAR}
-        rm ${TAR}
-        cd ${DIR}
-        ERRS=$(./configure -quiet)
-        if [[ -n ${ERRS} ]];
-        then
-            "Please review these error(s) before continuing."
-            echo ${ERRS}
-        else
-            cd -
-            tar czf ${TAR} ${DIR}
-            rm -rf ${DIR}
-        fi
-    fi
-}
 
 # get most recent make
 if [[ -z $(command -v make) || ${ALWAYS_DOWNLOAD} -eq 1 ]];
 then
+    echo "Getting make..."
     MAKE_MIRROR="${GNU_MIRROR}/make/"
     MAKE_VERISON=$(${CURL} ${MAKE_MIRROR} | grep -o make-[0-9\.]*tar\.gz | uniq | sort -V | tail -n1)
     MAKE_DOWNLOAD="${CURL} ${MAKE_MIRROR}/${MAKE_VERISON}"
@@ -44,22 +28,10 @@ then
     ${MAKE_DOWNLOAD} > ${MAKE_TAR}
 fi
 
-# get most recent monit
-if [[ -z $(command -v monit) || ${ALWAYS_DOWNLOAD} -eq 1 ]];
-then
-    MONIT_MIRROR="https://mmonit.com/monit"
-    MONIT_VERSION=$(${CURL} ${MONIT_MIRROR}#download | grep -oE 'dist\/monit-[0-9]+\.[0-9]+\.[0-9]+\.tar\.gz')
-    MONIT_DOWNLOAD="${CURL} ${MONIT_MIRROR}/${MONIT_VERSION}"
-    MONIT_TAR=$(echo ${MONIT_VERSION} | awk -F '/' '{print $NF}')
-    ${MONIT_DOWNLOAD} > ${MONIT_TAR}
-
-    # attempt to configure
-    configure ${MONIT_TAR}
-fi
-
-# get most recent python, python-utils, python-devel
+# get most recent python
 if [[ -z $(command -v python) || ${ALWAYS_DOWNLOAD} -eq 1 ]];
 then
+    echo "Getting python..."
     PY_MIRROR="https://www.python.org/ftp/python/"
     PY_VERSION_NUM=$(${CURL} ${PY_MIRROR} | grep -oE [0-9]+\.[0-9]+\.[0-9]+\/ | tail -n1)
     PY_MIRROR="${PY_MIRROR}/${PY_VERSION_NUM}"
@@ -67,7 +39,17 @@ then
     PY_DOWNLOAD="${CURL} ${PY_MIRROR}/${PY_VERSION}"
     PY_TAR=${PY_VERSION}
     ${PY_DOWNLOAD} > ${PY_TAR}
-
-    # attempt to configure
-    configure ${PY_TAR}
 fi
+
+# get most recent monit
+if [[ $(ls -l .. | grep monit | wc -l) -gt 0 && (-z $(command -v monit) || ${ALWAYS_DOWNLOAD} -eq 1) ]];
+then
+    echo "Getting monit..."
+    MONIT_MIRROR="https://mmonit.com/monit"
+    MONIT_VERSION=$(${CURL} ${MONIT_MIRROR}#download | grep -oE 'dist\/monit-[0-9]+\.[0-9]+\.[0-9]+\.tar\.gz')
+    MONIT_DOWNLOAD="${CURL} ${MONIT_MIRROR}/${MONIT_VERSION}"
+    MONIT_TAR=$(echo ${MONIT_VERSION} | awk -F '/' '{print $NF}')
+    ${MONIT_DOWNLOAD} > ${MONIT_TAR}
+    echo "madler/zlib" >> target
+fi
+
--- shared/offline/prepare-git-repo.sh
+++ shared/offline/prepare-git-repo.sh
@@ -1,58 +1,20 @@
 #!/usr/bin/env bash
 
-# read from file
-if [[ -f target ]];
+if [[ -n $1 ]];
 then
-    TARGET=$(cat target)
+    TARGETS="$@"
+elif [[ -f target ]];
+then
+    TARGETS=$(cat target)
 else
-    TARGET="$1"
-fi
-
-if [[ -z ${TARGET} ]];
-then
     echo "Pass the target repo as the first argument, including the organization."
     echo "ie sysstat/sysstat"
     exit 1
 fi
 
-TARGET_REPO=$(echo ${TARGET} | awk -F '/' '{print $NF}')
-TARGET_MASTER="${TARGET_REPO}-master"
-TARGET_TAR="${TARGET_MASTER}.tar.gz"
-OUTPUT_TAR="${TARGET_REPO}-make.tar.gz"
-
-# get package if needed
-if [[ ! -d "${TARGET_MASTER}" ]];
-then
+for TARGET in ${TARGETS};
+do
+    TARGET_REPO=$(echo ${TARGET} | awk -F '/' '{print $NF}')
+    TARGET_TAR="${TARGET_REPO}.tar.gz"
     curl -sSL https://github.com/${TARGET}/archive/master.tar.gz -o ${TARGET_TAR}
-    tar xf ${TARGET_TAR}
-fi
-cd ${TARGET_MASTER}
-
-# configure
-if [[ "$@" =~ --help ]];
-then
-    echo "Displaying configure help text"
-    echo ""
-    ./configure --help
-    echo ""
-    echo "Enter parameters to configure sysstat with."
-    echo "[Enter] to accept (leave blank for defaults), [Ctrl+C] to quit and continue later:"
-    read -p 'Parameters: ' PARAMS
-    CONFIGURE="./configure ${PARAMS}"
-else
-    CONFIGURE="./configure $@"
-fi
-
-ERRS=$(${CONFIGURE} --quiet)
-
-if [[ -n ${ERRS} ]];
-then
-    "Please review these error(s) before continuing."
-    echo "${ERRS}"
-else
-    # make a tar from the configured program
-    cd ..
-    tar czf ${OUTPUT_TAR} --transform "s/${TARGET_MASTER}/${TARGET_REPO}/" ${TARGET_MASTER}/
-    rm -rf ${TARGET_MASTER}
-    cd -
-fi
+done
