b64a789a893a423cde698117e552bdfdaf639691
gnegri <grantnegri@gmail.com>
2019-12-02 11:48:09 -0500

    remove old file

--- utils/split-configvars.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-AGENTS=$(cat new-agents | tr '|' '\n')
-for AGENT in ${AGENTS};
-do
-    echo "Setting up installer for ${AGENT}"
-    ./make-agent-installer.sh --build --git-add ${AGENT}
-done
