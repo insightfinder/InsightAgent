4a7f66b73f29d6f60de470486c7bb180afaaa146
gnegri <grantnegri@gmail.com>
2020-01-07 15:13:59 -0500

    tidying up

--- sar/install.sh
+++ sar/install.sh
@@ -88,8 +88,6 @@ ${SHOPT_NOCASEMATCH}
 if is_dry_run;
 then
     echo "Running in dry-run mode. Run with '-c' or '--create' flag to execute the changes below."
-else
-    read -p "Running in commit mode. Press [Enter] to continue, or [Ctrl+C] to quit"
 fi
 
 # Python 3 compatability
