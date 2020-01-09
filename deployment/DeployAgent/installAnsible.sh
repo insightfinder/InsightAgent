c07856ba8b366f2afe40306125dba4aaed19624a
gnegri <grantnegri@gmail.com>
2020-01-09 16:54:49 -0500

    try to get right version of ansible

--- deployment/DeployAgent/installAnsible.sh
+++ deployment/DeployAgent/installAnsible.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
+INSTALL=0
 if [ ! $(which ansible-playbook) ]; then
-  if [ -f /etc/centos-release ] || [ -f /etc/redhat-release ] || [ -f /etc/oracle-release ] || [ -f /etc/system-release ] || grep -q 'Amazon Linux' /etc/system-release; then
-    # sudo yum install -y epel-release
-    sudo yum install -y ansible
-    LIST_VERSIONS="sudo yum --showduplicates list ansible"
-  elif [ -f /etc/debian_version ] || [ grep -qi ubuntu /etc/lsb-release ] || grep -qi ubuntu /etc/os-release; then
-    apt-get -y update
-    apt-get -y install software-properties-common
-    apt-add-repository -y ppa:ansible/ansible
-    apt-get -y update
-    apt-get install -y ansible
-    LIST_VERSIONS="sudo apt-cache policy ansible"
-  else
-    echo 'WARN: Could not detect distro or distro unsupported'
-    echo 'WARN: Trying to install ansible via pip without some dependencies'
-    echo 'WARN: Not all functionality of ansible may be available'
-    exit 1
-  fi
-
+    INSTALL=1
 fi
 
-echo "Installed ansible. Please check that the version is <=2.7:"
+if [ -f /etc/centos-release ] || [ -f /etc/redhat-release ] || [ -f /etc/oracle-release ] || [ -f /etc/system-release ] || grep -q 'Amazon Linux' /etc/system-release; then
+    LIST_VERSIONS="sudo yum --showduplicates list ansible"
+    if [[ ${INSTALL} -eq 1 ]]; then
+        # sudo yum install -y epel-release
+        sudo yum install -y ansible
+    fi
+elif [ -f /etc/debian_version ] || [ grep -qi ubuntu /etc/lsb-release ] || grep -qi ubuntu /etc/os-release; then
+    LIST_VERSIONS="sudo apt-cache policy ansible"
+    if [[ ${INSTALL} -eq 1 ]]; then
+        apt-get -y update
+        apt-get -y install software-properties-common
+        apt-add-repository -y ppa:ansible/ansible
+        apt-get -y update
+        apt-get install -y ansible
+    fi
+else
+    echo 'WARN: Could not detect distro or distro unsupported'
+    exit 1
+fi
+
+if [[ ${INSTALL} -eq 1 ]]; then
+    echo "Installed ansible."
+else
+    echo "Ansible already installed."
+fi
+
+echo "Please check that the version is <=2.7:"
 echo "$(ansible --version)"
-echo "Otherwise, check available versions with\n${LIST_VERSIONS}"
+echo "Otherwise, check available versions with"
+echo "${LIST_VERSIONS}"
 
 exit 0
