8705eeb8afe2954b1b588694a91b96c386b22ae3
zhangzinan <geek@zznland.com>
2020-08-14 11:03:08 +0800

    ii-7199 add py3 support for logfilereplay and metricfilereplay

--- logFileReplay/send_data_py3.py
+++ logFileReplay/send_data_py3.py
@@ -1,21 +1,22 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import json
 import os
-import socket
 import sys
 import time
-import requests
-from sys import getsizeof
+import copy
 import urllib3
-from ConfigParser import SafeConfigParser
+import importlib
+from sys import getsizeof
+from configparser import ConfigParser
+
+import requests
 
 
 def get_agent_config_vars():
     config_vars = {}
     try:
         if os.path.exists(os.path.join(os.getcwd(), "config.ini")):
-            parser = SafeConfigParser()
+            parser = ConfigParser()
             parser.read(os.path.join(os.getcwd(), "config.ini"))
             file_name = parser.get('InsightFinder', 'file_name')
             license_key = parser.get('InsightFinder', 'insightFinder_license_key')
@@ -23,19 +24,19 @@ def get_agent_config_vars():
             user_name = parser.get('InsightFinder', 'insightFinder_user_name')
             server_url = parser.get('InsightFinder', 'insightFinder_server_url')
             if len(file_name) == 0:
-                print "Agent not correctly configured(file name). Check config file."
+                print("Agent not correctly configured(file name). Check config file.")
                 sys.exit(1)
             if len(license_key) == 0:
-                print "Agent not correctly configured(license key). Check config file."
+                print("Agent not correctly configured(license key). Check config file.")
                 sys.exit(1)
             if len(project_name) == 0:
-                print "Agent not correctly configured(project name). Check config file."
+                print("Agent not correctly configured(project name). Check config file.")
                 sys.exit(1)
             if len(user_name) == 0:
-                print "Agent not correctly configured(user name). Check config file."
+                print("Agent not correctly configured(user name). Check config file.")
                 sys.exit(1)
             if len(server_url) == 0:
-                print "Agent not correctly configured(server url). Check config file."
+                print("Agent not correctly configured(server url). Check config file.")
                 sys.exit(1)
             config_vars['file_name'] = file_name
             config_vars['license_key'] = license_key
@@ -43,7 +44,7 @@ def get_agent_config_vars():
             config_vars['user_name'] = user_name
             config_vars['server_url'] = server_url
     except IOError:
-        print "config.ini file is missing"
+        print("config.ini file is missing")
     return config_vars
 
 
@@ -57,7 +58,7 @@ def send_data(metric_data):
     to_send_data_dict["licenseKey"] = config_vars['license_key']
     to_send_data_dict["projectName"] = config_vars['project_name']
     to_send_data_dict["userName"] = config_vars['user_name']
-    to_send_data_dict["agentType"] = "MetricFileReplay"
+    to_send_data_dict["agentType"] = "LogFileReplay"
 
     to_send_data_json = json.dumps(to_send_data_dict)
 
@@ -65,33 +66,28 @@ def send_data(metric_data):
     post_url = config_vars['server_url'] + "/customprojectrawdata"
     response = requests.post(post_url, data=json.loads(to_send_data_json), verify=False)
     if response.status_code == 200:
-        print str(len(bytearray(to_send_data_json))) + " bytes of data are reported."
+        print(str(getsizeof(to_send_data_json)) + " bytes of data are reported.")
     else:
-        print "Failed to send data."
-    print "--- Send data time: %s seconds ---" + str(time.time() - send_data_time)
+        print("Failed to send data.")
+    print("--- Send data time: %s seconds ---" + str(time.time() - send_data_time))
 
 
 if __name__ == "__main__":
     urllib3.disable_warnings()
-    reload(sys)
-    sys.setdefaultencoding('utf-8')
+    importlib.reload(sys)
     config_vars = get_agent_config_vars()
-    # chunk size is 2Mb
-    CHUNK_SIZE = 2 * 1024 * 1024
+    CHUNK_SIZE = 1000
     with open(config_vars["file_name"]) as json_data:
+        d = json.load(json_data)
         data = []
         count = 0
-        header_str = json_data.readline()
-        header = map(lambda x: x.strip(), header_str.split(','))
-        for line in json_data:
-            entry = map(lambda x: x.strip(), line.split(','))
-            new_entry = dict(zip(header, entry))
+        for entry in d:
+            new_entry = copy.deepcopy(entry)
             data.append(new_entry)
             count += 1
-            if count % 50 == 0 and getsizeof(data) >= CHUNK_SIZE:
+            if count >= CHUNK_SIZE:
                 send_data(data)
+                count = 0
                 data = []
-        if len(data) != 0:
+        if count != 0:
             send_data(data)
-
-    print "--- Total count: %s ---" + str(count)
