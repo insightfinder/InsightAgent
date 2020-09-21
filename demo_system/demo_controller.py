03b84549d5bc6005b6c5bd3c267134d61d7f8ddc
Cai Xu <cai@insightfinder.com>
2020-09-21 13:29:11 -0400

    add demo system

---
+++ demo_system/demo_controller.py
@@ -0,0 +1,89 @@
+import constant
+import utility
+import datetime
+from configparser import SafeConfigParser
+from optparse import OptionParser
+
+
+def get_parameters():
+    parser = OptionParser(usage="Usage: %prog [options]")
+    parser.add_option("-l", "--license_key",
+                      action="store", dest="license_key", help="User's license key")
+    parser.add_option("-u", "--user_name",
+                      action="store", dest="user_name", help="User account's name")
+    parser.add_option("-s", "--server_url",
+                      action="store", dest="server_url", help="Server url to stream data")
+    parser.add_option("-d", "--data_type",
+                      action="store", dest="data_type", help="The data type of the data to stream")
+    (options, args) = parser.parse_args()
+    parameters = {}
+    if options.license_key is not None:
+        parameters[constant.LICENSE_KEY] = options.license_key
+    if options.user_name is not None:
+        parameters[constant.USER_NAME] = options.user_name
+    if options.server_url is not None:
+        parameters[constant.SERVER_URL] = options.server_url
+    if options.data_type is not None:
+        parameters[constant.DATA_TYPE] = options.data_type
+    else:
+        parameters[constant.DATA_TYPE] = None
+    return parameters
+
+
+def get_current_time():
+    current_time = datetime.datetime.now()
+    epoch = datetime.datetime.utcfromtimestamp(0)
+    timestamp = int((current_time - epoch).total_seconds()) * 1000
+    return (timestamp // constant.MINUTE) * constant.MINUTE
+
+def get_current_date_minute():
+    return datetime.datetime.now().strftime(constant.DATE_TIME_FORMAT_MINUTE)
+
+
+def modified_config_file():
+    user_name = parameters[constant.USER_NAME]
+    config_file_name = utility.get_config_file_name(user_name)
+    config = SafeConfigParser()
+    config.read(config_file_name)
+    current_data_type = config[constant.IF][constant.DATA_TYPE]
+    if current_data_type == 'abnormal' and parameters[constant.DATA_TYPE] == 'normal':
+        #Switch to normal data and trigger the reverse development
+        config[constant.IF][constant.REVERSE_DEPLOYMENT] = 'True'
+        config[constant.IF][constant.NORMAL_TIME] = get_current_date_minute()
+    if current_data_type == 'normal':
+        #Swtich to abnormal data
+        config[constant.IF][constant.ABNORMAL_TIME] = get_current_date_minute()
+    config[constant.IF][constant.DATA_TYPE] = parameters[constant.DATA_TYPE]
+    utility.save_config_file(config_file_name, config)
+
+
+def generate_config_file():
+    user_name = parameters[constant.USER_NAME]
+    config_file_name = utility.get_config_file_name(user_name)
+    time = get_current_date_minute()
+    config = SafeConfigParser()
+    config[constant.IF] = {constant.LICENSE_KEY: parameters[constant.LICENSE_KEY],
+                           constant.USER_NAME: user_name,
+                           constant.SERVER_URL: parameters[constant.SERVER_URL],
+                           constant.START_TIME: time,
+                           constant.DATA_TYPE: constant.DATA_TYPE_NORMAL,
+                           constant.REVERSE_DEPLOYMENT: 'False',
+                           constant.NORMAL_TIME: time,
+                           constant.ABNORMAL_TIME: 0}
+    config[constant.LOG] = {constant.PROJECT_NAME: constant.LOG_PROJECT_NAME}
+    config[constant.DEPLOYMENT] = {constant.PROJECT_NAME: constant.DEPLOYMENT_PROJECT_NAME}
+    config[constant.WEB] = {constant.PROJECT_NAME: constant.WEB_PROJECT_NAME}
+    config[constant.METRIC] = {constant.PROJECT_NAME: constant.METRIC_PROJECT_NAME}
+    utility.save_config_file(config_file_name, config)
+
+
+def is_initialized():
+    return parameters[constant.DATA_TYPE] is None
+
+
+if __name__ == "__main__":
+    parameters = get_parameters()
+    if is_initialized():
+        generate_config_file()
+    else:
+        modified_config_file()
---
+++ demo_system/demo_cron.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while true; do
+       echo "Start trigger demo.py"
+       python data_replay.py -u demoUser
+       sleep 60
+done
