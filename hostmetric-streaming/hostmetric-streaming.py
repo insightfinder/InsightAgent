e507b9bf700bd776a9e54017de54a24e9dd624f1
Bo Du <bo@insightfinder.com>
2021-02-05 15:41:53 +0800

    Finished filesystem metric ingestion

--- hostmetric-streaming/hostmetric-streaming.py
+++ hostmetric-streaming/hostmetric-streaming.py
@@ -9,20 +9,51 @@ import argparse
 import json
 import logging
 import logging.handlers
+import os
 import time
 
 import pandas as pd
 import requests
 import yaml
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 
-def send_data(data, config):
+def send_request(url, payload):
     """
-    Send metric data to insightfinder
-    :param data: a list conforming to the metricData field of insightfinder data ingestion api,
+    Send http request.
+    :param url: The url to access.
+    :param payload: The payload to send.
+    :return: None if request failed, Response content if request ok.
+    """
+
+    logging.debug(f'Request {url} with payload:\n{payload}')
+
+    retries = Retry(
+        total=10,
+        allowed_methods=False,  # Set to a False value to retry on any verb.
+        status_forcelist=[401, 500, 502, 503, 504],
+        backoff_factor=3
+    )
+
+    with requests.Session() as s:
+        s.mount(url, HTTPAdapter(max_retries=retries))
+        try:
+            resp = s.post(url, data=payload, verify=False, timeout=5)
+            if resp.ok:
+                return resp.text
+        except requests.exceptions.RequestException as e:
+            logging.error(str(e))
+        return None
+
+
+def send_data(data, if_config):
+    """
+    Send metric data to insightfinder.
+    :param data: A list conforming to the metricData field of insightfinder data ingestion api,
                 ex. [{"timestamp":"1558631315000", "DiskUsed[build-server-7]":"0.253"}]
-    :param config: a dict containing all fields except metricData for insightfinder data ingestion api
-    :return: true/false to show if data sent
+    :param if_config: A dict containing all fields except metricData for insightfinder data ingestion api
+    :return: True/False to show if data sent
     """
 
     if not data:
@@ -30,32 +61,56 @@ def send_data(data, config):
         return
 
     payload = {'metricData': json.dumps(data),
-               'agentType': config['insightfinder']['agent_type'],
-               'projectName': config['insightfinder']['project_name'],
-               'userName': config['insightfinder']['user_name'],
-               'licenseKey': config['insightfinder']['license_key']}
+               'agentType': if_config['agent_type'],
+               'projectName': if_config['project_name'],
+               'userName': if_config['user_name'],
+               'licenseKey': if_config['license_key']}
 
-    url = config['insightfinder']['appserver'] + '/customprojectrawdata'
-    logging.debug(f'Payload for {url}:\n{payload}')
+    url = if_config['appserver'] + '/customprojectrawdata'
 
-    try:
-        r = requests.post(url, data=payload, verify=False, timeout=3)
-        if r.status_code == 200:
-            logging.info(f'Succeeded to send data to {url} for project: {payload["projectName"]}')
+    content = send_request(url, payload)
+    if content:
+        logging.info(f'Succeeded to send data to {url} for project: {payload["projectName"]}')
+        return True
+    else:
+        logging.error(f'Failed to send data to {url} for project: {payload["projectName"]}')
+        return False
+
+
+def joined_bylevel(df, level, surround, suffix=True):
+    """
+    Join dataframe by one level.
+    :param df: Dataframe.
+    :param level: The level to group by.
+    :param surround: String of a pair of symbols to surround the item name of grouped level, which used to rename column.
+    :param suffix: True to add suffix, False to add prefix to column label.
+    :return: Joined dataframe.
+    """
+
+    grouped = df.groupby(level)
+
+    # Prepare empty dataframe with same index as joined dataframe
+    first = list(grouped.groups.keys())[0]
+    idx_bylevel = df.xs(first, level=level).index
+    joined_df = pd.DataFrame(index=idx_bylevel)
+    for name, group in grouped:
+        if suffix:
+            renamed_df = df.xs(name, level=level).add_suffix(surround[0] + name + surround[1])
         else:
-            logging.error(f'Failed to send data to {url} for project: {payload["projectName"]}')
-    except requests.exceptions.RequestException as e:
-        logging.error(str(e))
+            renamed_df = df.xs(name, level=level).add_prefix(surround[0] + name + surround[1])
+        joined_df = joined_df.join(renamed_df)
+
+    return joined_df
 
 
-def build_data(data, metricspec):
+def build_data(data, schema):
     """
     Build a list conforming to the metricData field of insightfinder data ingestion api
-    :param data: a list of metric dict, ex.
+    :param data: A json string of metric data, ex.
                 [{'TIME': '2021-01-07 13:03:30', 'IP_ADDRESS': '10.222.55.55', 'CPU_USER': '0.003', 'CPU_SYSTEM': '0'},
                 {'TIME': '2021-01-07 13:03:30', 'IP_ADDRESS': '10.222.55.54', 'CPU_USER': '0.033', 'CPU_SYSTEM': '0'}]
-    :param metricspec: a dict of metric data specification, refer to the comments in config.yml
-    :return: a list of metric data for insightfiner data ingestion api,
+    :param schema: A dict of data schema, refer to the comments in config.yml
+    :return: A list of metric data for insightfiner data ingestion api,
             ex. [{"timestamp":"1558631315000", "DiskUsed[build-server-7]":"0.253"}]
     """
 
@@ -63,58 +118,45 @@ def build_data(data, metricspec):
         logging.warning('No data to build.')
         return None
 
-    logging.debug(f'Building data: {data}')
-    logging.debug(f'with spec: {metricspec["fields"]}')
+    logging.debug(f'Building data:\n{data}')
+    logging.debug(f'with schema:\n{schema["fields"]}')
 
-    df = pd.DataFrame(data)
-    df = df.applymap(str)  # ensure all metric value is string
+    df = pd.read_json(data, dtype='str')
+    if df.empty:
+        logging.warning('Empty dataframe.')
+        return None
 
-    col_timestamp = metricspec['fields']['timestamp']
-    col_instance = metricspec['fields']['instance']
-    col_metrics = metricspec['fields']['metrics']
+    col_timestamp = schema['fields']['timestamp']
+    col_instance = schema['fields']['instance']
+    col_metrics = schema['fields']['metrics']
     col_list = [col_timestamp, col_instance] + col_metrics
-    logging.debug(f'Columns to process: {col_list}')
+    col_device = None
+    if 'device' in schema['fields']:
+        col_device = schema['fields']['device']
+        col_list.append(col_device)
+        devices = schema['devices']
 
+    # Get needed columns
     df = df[col_list]
     df[col_timestamp] = pd.to_datetime(df[col_timestamp])
 
-    '''
-    At any time point, there are multiple instances which have its metrics.
-    To create MultiIndex, set timestamp as level 0, instance as level 1.
-    For example:
-                                     CPU_USER CPU_SYSTEM CPU_WAIT CPU_IDLE
-    timestamp           instance
-    2021-01-07 12:59:30 10.222.55.54    0.012      0.002    0.006    0.979
-                        10.222.55.55     0.02      0.001    0.003    0.973
-    2021-01-07 13:01:30 10.222.55.54    0.004          0    0.023    0.972
-                        10.222.55.55    0.028          0    0.093    0.877
-    '''
-    df.reset_index()
-    df.set_index([col_timestamp, col_instance], inplace=True)
+    # Set MultiIndex
+    idx = [col_timestamp, col_instance]
+    if col_device:
+        idx.append(col_device)
+        df = df[df[col_device].isin(devices)]
+    df.set_index(idx, inplace=True)
     df.sort_index(inplace=True)
 
-    '''
-    Start to join each instance's dataframe to get one dataframe as a whole like below:
-                        CPU_USER[10.222.55.54] ... CPU_SYSTEM[10.222.55.55]
-    timestamp
-    2021-01-07 12:59:30                  0.001 ...                    0.012
-    2021-01-07 13:01:30                  0.013 ...                    0.009
-    '''
-    grouped_instance = df.groupby(col_instance)
-    logging.debug(f'Grouped instance: {grouped_instance.groups.keys()}')
-
-    # Empty dataframe must have same index as joined dataframe
-    first_instance = list(grouped_instance.groups.keys())[0]
-    curr_idx = df.xs(first_instance, level=col_instance).index
-    whole_df = pd.DataFrame(index=curr_idx)
-    for name, group in grouped_instance:
-        # Rename column to form "<metric name>[<instance name>]"
-        instance_df = df.xs(name, level=col_instance).add_suffix('[' + name + ']')
-        whole_df = whole_df.join(instance_df)
+    if col_device:
+        joined_df = joined_bylevel(df, col_device, surround='()')
+        final_df = joined_bylevel(joined_df, col_instance, surround='[]')
+    else:
+        final_df = joined_bylevel(df, col_instance, surround='[]')
 
     # Build metricData
     metric_data = []
-    for ts, metrics in whole_df.to_dict('index').items():
+    for ts, metrics in final_df.to_dict('index').items():
         epoch = int(ts.timestamp() * 1000)
         d = {'timestamp': str(epoch)}
         d.update(metrics)
@@ -123,39 +165,34 @@ def build_data(data, metricspec):
     return metric_data
 
 
-def query_data(metricset, freq, instances):
+def query_data(metricset, period, instances):
     """
     Query metric data
-    :param metricset: metricset to query, ex. cpu, filesystem.
-    :param freq: the frequency to query metric, in minutes.
-    :param instances: a list of instance name for which to query metric.
-    :return: a json string of queried metric data
+    :param metricset: The metricset to query, ex. cpu, filesystem.
+    :param period: The past time period to query, in minutes.
+    :param instances: A list of instance name for which to query metric.
+    :return: A json string of queried metric data
     """
 
-    start_time = (pd.Timestamp.now() - pd.Timedelta(freq, 'minutes')).strftime('%Y%m%d%H%M%S')
+    start_time = (pd.Timestamp.now() - pd.Timedelta(period * 2, 'minutes')).strftime('%Y%m%d%H%M%S')
     end_time = pd.Timestamp.now().strftime('%Y%m%d%H%M%S')
     payload = {'param': metricset, 'ips': ','.join(instances),
                'startTime': start_time, 'endTime': end_time}
 
     url = 'http://10.9.131.38/external/getdata'
-    logging.debug(f'Payload for {url}:\n{payload} ')
 
-    try:
-        r = requests.get(url, params=payload, timeout=3)
-        if r.ok:
-            logging.debug(f'Get {metricset} data: {r.json()}')
-            return r.json()
-        else:
-            logging.error(f'Failed to query {metricset} data')
-    except requests.exceptions.RequestException as e:
-        logging.error(str(e))
+    content = send_request(url, payload)
+    if content:
+        logging.debug(f'Get {metricset} data:\n{content}')
+    else:
+        logging.error(f'Failed to query {metricset} data')
 
-    return None
+    return content
 
 
 if __name__ == '__main__':
     # Command line arguments
-    parser = argparse.ArgumentParser(description='Get cmcc host metric')
+    parser = argparse.ArgumentParser(description='Streaming host metric')
     parser.add_argument('-v', '--verbosity', action='count', default=0,
                         help='increase output verbosity')
     parser.add_argument('-l', '--logfile', help='logging file')
@@ -168,35 +205,33 @@ if __name__ == '__main__':
         loglevel = logging.INFO
     else:
         loglevel = logging.WARNING
-    fmt_str = '%(asctime)s - %(levelname)s - %(filename)s[line:%(lineno)d] - %(message)s'
+    fmt_str = '%(asctime)s - %(levelname)s - %(filename)s[line:%(lineno)d] ==> %(message)s'
     if args.logfile:
         logger = logging.getLogger()
         logger.setLevel(loglevel)
-        rh = logging.handlers.RotatingFileHandler(args.logfile, maxBytes=5*1024**2, backupCount=1)
+        rh = logging.handlers.RotatingFileHandler(args.logfile, maxBytes=5 * 1024 ** 2, backupCount=1)
         formatter = logging.Formatter(fmt_str)
         rh.setFormatter(formatter)
         logger.addHandler(rh)
     else:
-        logging.basicConfig(
-            format=fmt_str,
-            level=loglevel)
+        logging.basicConfig(format=fmt_str, level=loglevel)
 
     # Load config.yml
-    with open('config.yml') as c:
+    config_path = f'{os.path.dirname(os.path.realpath(__file__))}/config.yml'
+    with open(config_path) as c:
         config = yaml.load(c, yaml.FullLoader)
-        logging.debug(f'Load config: {config}')
+        logging.debug(f'Load config:\n{config}')
         instances = config['instances']
         metricset = config['metricset']
         query_interval = config['query_interval']
-        logging.debug(f'To query {metricset} for {len(instances)} instances.')
+        logging.debug(f'To query {len(metricset)} metricsets for {len(instances)} instances.')
 
     while True:
-        logging.info('==> Start to process')
+        logging.info('Start to process')
         start_time = time.perf_counter()
-        for name,spec in metricset.items():
-            r = query_data(name, query_interval, instances)
-            metric_data = build_data(r, spec)
-            send_data(metric_data, config)
+        for name, schema in metricset.items():
+            d = query_data(name, query_interval, instances)
+            send_data(build_data(d, schema), config['insightfinder'])
         elapsed_time = time.perf_counter() - start_time
-        logging.info(f'==> Finished in {elapsed_time} seconds, to sleep {query_interval} minutes.\n...')
+        logging.info(f'Finished in {elapsed_time} seconds, to sleep {query_interval} minutes.\n...')
         time.sleep(query_interval * 60)
