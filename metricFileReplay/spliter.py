308baa8d27d20d37fe2cdf015d14174661a9d47b
mingyu <frank.zhu.mingyu@gmail.com>
2021-03-05 10:33:37 -0500

    split csv into multiple files per service alias

---
+++ metricFileReplay/spliter.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import logging
+from csv import reader
+
+
+def split_file(file_name, output='out'):
+    with open(file_name) as json_data:
+        logging.info(f"open {file_name}...")
+        count = 0
+        data = {}
+        csv_reader = reader(json_data)
+        header = next(csv_reader)  # skip header 
+
+        for line in csv_reader:
+            time_bucket, service_alias, client_alias, http_status, svc_mean, tx_mean, \
+                req_count, value = line
+            
+            if service_alias not in data:
+                data[service_alias] = ','.join(line)
+
+            else:
+                data[service_alias] += '\n' + ','.join(line)
+            
+    logging.info(f"found {len(data)} service alias")
+    for key in data.keys():
+        with open(os.path.join(output, key + '.csv'), 'a') as f:
+            f.write(data[key])
+
+
+def main():
+    for file_name in sys.argv[1:]:
+        split_file(file_name)
+    
+
+if __name__ == "__main__":
+    main()
+
