7833c5102655024f3c725a083c78d17393e2126a
mingyu <frank.zhu.mingyu@gmail.com>
2021-03-18 13:50:05 -0400

    support multiple projects and different intervals

--- dell/anomaly_alert.py
+++ dell/anomaly_alert.py
@@ -1,13 +1,20 @@
-import requests
-import json
-import pickle
-import logging
 import configparser
-from dateutil.tz import gettz
-from datetime import datetime, timedelta, date, time, timezone
+from datetime import datetime, date, timedelta, timezone, time as datetime_time
+import json
+import logging
+import pickle
+import requests
+import time
 
 
-logging.basicConfig(level=logging.WARNING)
+logging.basicConfig(level=logging.DEBUG,
+                    format=('%(asctime)-15s'
+                            '%(filename)s: '
+                            '%(levelname)s: '
+                            '%(funcName)s(): '
+                            '%(lineno)d:\t'
+                            '%(message)s')
+                        )
 
 
 def get_anomaly_data(host, data):
@@ -20,42 +27,39 @@ def get_anomaly_data(host, data):
         result = resp.json()
     except Exception as e:
         logging.WARNING(e)
-
     return result
 
 
-def get_anomaly_events(start, end, events_sent, host, profile, licenseKey):
+def get_anomaly_events(start, end, events_sent, host, projects, user, licenseKey):
+    """ given start and end time, get anomaly events for a project """
     startTime = int(start.timestamp()*1000)
     endTime = int(end.timestamp()*1000)
     logging.debug(startTime)
     logging.debug(endTime)
     events = []
-    proj_name, user_name = profile.split('@')
 
-    data = {"projectName": proj_name, "transferToProjectName": "dummy", "transferToCustomerName": "user",
-            "startTime": startTime, "endTime": endTime, "licenseKey": licenseKey}
-    user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36"
+    for proj_name in projects:
+        print("project:", proj_name)
+        data = {"projectName": proj_name, "transferToProjectName": "dummy", "transferToCustomerName": "user",
+                "startTime": startTime, "endTime": endTime, "licenseKey": licenseKey, "userName": user}
 
-    data.update({"userName": user_name})
+        anomaly_data = get_anomaly_data(host, data)
+        if len(anomaly_data) == 0:
+            continue
 
-    anomaly_data = get_anomaly_data(host, data)
-    if len(anomaly_data) == 0:
-        return events
-
-    for d in anomaly_data['DATA']['anomalyEventsList']:
-        for e in json.loads(d['rootCauseJson'])['rootCauseDetailsArr']:
-            instance = e.get('instanceId')
-            metric = e.get('rootCauseMetric')
-            for event_time in e.get('timePairArr'):
-                event_start = str(event_time.get('startTimestamp'))
-                key = f"{instance}-{metric}"
-                if key not in events_sent.get(start, ''):
-                    logging.debug(f"events:{events_sent} {start} {key}")
-                    events_sent[start].add(key)
-                    event = f"""{metric} ({e.get('metricValue')}) is {e.get('pct')}% {e.get('direction')} \
-than normal at {instance}"""
-                    logging.debug(event)
-                    events.append((instance, event))
+        for d in anomaly_data['DATA']['anomalyEventsList']:
+            for e in json.loads(d['rootCauseJson'])['rootCauseDetailsArr']:
+                instance = e.get('instanceId')
+                metric = e.get('rootCauseMetric')
+                for event_time in e.get('timePairArr'):
+                    event_start = event_time.get('startTimestamp')
+                    key = f"{instance}-{metric}"
+                    if key not in events_sent:
+                        events_sent.update({key: event_start})
+                        event = f"""{metric} ({e.get('metricValue')}) is {e.get('pct')}% {e.get('direction')} \
+    than normal at {instance}"""
+                        events.append((instance, event))
+                        logging.debug(f"add event:{instance} {event}")
 
     return events
 
@@ -66,7 +70,7 @@ def send_alert(instance, event, url):
     try:
         data = {
             "fqdn": instance,
-            "severity": 3,
+            "severity": 4,
             "message": event,
             "complete": "false"
         }
@@ -78,19 +82,25 @@ def send_alert(instance, event, url):
 
 
 def main():
-    today = datetime.combine(date.today(), time(), tzinfo=timezone.utc)
+    today = datetime.combine(date.today(), datetime_time(), tzinfo=timezone.utc)
     tomorrow = today + timedelta(days=1)
-    logging.info(f"today={today}")
+    start_time = time.time()
+    logging.info(f"check anomaly in {today}")
 
-    events_sent = {today:set()}
+    events_sent = {}
 
     config = configparser.ConfigParser()
+    projects = []
+    interval = 24
+
     try:
         config.read('cfg.ini')
         report_url = config['DEFAULT']['report_url']
         host = config['DEFAULT']['host']
-        profile = config['DEFAULT']['profile']
+        projects = config['DEFAULT']['project'].split(',')
+        user = config['DEFAULT']['user']
         licenseKey = config['DEFAULT']['licenseKey']
+        interval = int(config['DEFAULT']['interval'])
     except Exception as e:
         logging.warning(e)
 
@@ -101,19 +111,21 @@ def main():
     except:
         pass
 
-    logging.debug(f"today total events: {len(events_sent.get(today, ''))}")
+    logging.debug(f"today total events: {len(events_sent)}")
 
-    for instance, event in get_anomaly_events(today, tomorrow, events_sent, host, profile, licenseKey):
+    for instance, event in get_anomaly_events(today, tomorrow, events_sent, host, projects, user, licenseKey):
         send_alert(instance, event, report_url)
 
     # clean up records
-    if len(events_sent) > 1:
-        for day in events_sent.keys():
-            if today - day > timedelta(days=1):
-                events_sent.pop(day)
+    utc_now = datetime.utcnow().timestamp()
+    for k, v in events_sent.items():
+        if utc_now - v/1000 > interval * 3600:
+            events_sent.pop(k)
 
     with open("events_records", "wb") as f:
         pickle.dump(events_sent, f)
+
+    logging.info(f"done in {time.time()-start_time:8.3f} secs")
 
 if __name__ == '__main__':
     main()
