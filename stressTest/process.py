01f47f0c89cf5b23bb03a1e7581298a8bca34429
nathanb1248 <nathanb@insightfinder.com>
2022-03-28 15:36:09 -0400

    removed debug comments

--- stressTest/process.py
+++ stressTest/process.py
@@ -3,19 +3,19 @@ import sys
 import time
 
 pid = int(sys.argv[1])
-timeout = int(sys.argv[2]) #100
-percent = int(sys.argv[3]) #50
-interval = int(sys.argv[4]) #10
+timeout = int(sys.argv[2])
+percent = int(sys.argv[3])
+interval = int(sys.argv[4])
 
 print('Starting Process: %d' % pid)
 
-chunk = percent/interval #5
-chunkTime = timeout/interval #10
+chunk = percent/interval
+chunkTime = timeout/interval
 increment = chunk
 
-end = time.time() + float(timeout)  # X minutes from now
+end = time.time() + float(timeout)
 
-incrementTime = time.time() + float(chunkTime) #current time plus 10 seconds
+incrementTime = time.time() + float(chunkTime)
 
 while time.time() < end:
     busyPercent = float(increment)/100
