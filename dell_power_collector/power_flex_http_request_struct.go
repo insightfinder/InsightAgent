5168d5d56101220f3a929340a6be07de66c3045c
Hao Fu <hao@insightfinder.com>
2023-06-05 17:23:12 +0800

    II-14362 fix auth bug

--- dell_power_collector/power_flex_http_request_struct.go
+++ dell_power_collector/power_flex_http_request_struct.go
@@ -5,6 +5,12 @@ type AuthRequest struct {
 	UserName string `json:"userName"`
 }
 
+type FlexManagerAuthRequest struct {
+	Password string `json:"password" validate:"required"`
+	UserName string `json:"userName" validate:"required"`
+	Domain   string `json:"domain" validate:"required"`
+}
+
 type AuthResponse struct {
 	ApiKey    string `json:"apiKey" validate:"required"`
 	ApiSecret string `json:"apiSecret" validate:"required"`
