eeab520d4c775f74507cfe23dd6287526f87b236
Hao Fu <hao@insightfinder.com>
2023-06-05 18:44:44 +0800

    II-14362 add chunk number

--- dell_power_collector/log_data_struct.go
+++ dell_power_collector/log_data_struct.go
@@ -7,12 +7,14 @@ type LogData struct {
 }
 
 type LogDataReceivePayload struct {
-	UserName         string    `json:"userName" validate:"required"`
-	ProjectName      string    `json:"projectName" validate:"required"`
-	LicenseKey       string    `json:"licenseKey" validate:"required"`
-	LogDataList      []LogData `json:"metricData" validate:"required"`
-	SystemName       string    `json:"systemName,omitempty"`
-	MinTimestamp     int64     `json:"minTimestamp,omitempty"`
-	MaxTimestamp     int64     `json:"maxTimestamp,omitempty"`
-	InsightAgentType string    `json:"agentType,omitempty"`
+	UserName          string    `json:"userName" validate:"required"`
+	ProjectName       string    `json:"projectName" validate:"required"`
+	LicenseKey        string    `json:"licenseKey" validate:"required"`
+	LogDataList       []LogData `json:"metricData" validate:"required"`
+	SystemName        string    `json:"systemName,omitempty"`
+	MinTimestamp      int64     `json:"minTimestamp,omitempty"`
+	MaxTimestamp      int64     `json:"maxTimestamp,omitempty"`
+	InsightAgentType  string    `json:"agentType,omitempty"`
+	ChunkSerialNumber int64     `json:"chunkSerialNumber,omitempty"`
+	ChunkTotalNumber  int64     `json:"chunkTotalNumber,omitempty"`
 }
