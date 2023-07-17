b9e1ccbc31031b52c1f4309b7ae9e4fe0d8feea0
maoyuwang <i@maoyu.wang>
2023-07-17 12:47:43 -0400

    Draft

--- kubernetes-agent/go.mod
+++ kubernetes-agent/go.mod
@@ -2,6 +2,9 @@ module kubernetes-agent
 
 go 1.20
 
-require github.com/carlmjohnson/requests v0.23.4
+require (
+	github.com/bigkevmcd/go-configparser v0.0.0-20230427073640-c6b631f70126
+	github.com/carlmjohnson/requests v0.23.4
+)
 
 require golang.org/x/net v0.7.0 // indirect
--- kubernetes-agent/main.go
+++ kubernetes-agent/main.go
@@ -15,14 +15,20 @@ func main() {
 
 	Now := time.Now()
 	Before := Now.Add(-time.Minute * 10)
-	PrometheusServer.GetMetricData("CPU", Before, Now)
-	//MemoryData := PrometheusServer.GetMetricData("Memory", NOW)
-	//DiskReadData := PrometheusServer.GetMetricData("DiskRead", NOW)
-	//DiskWriteData := PrometheusServer.GetMetricData("DiskWrite", NOW)
-	//NetworkInData := PrometheusServer.GetMetricData("DiskWrite", NOW)
-	//NetworkOutData := PrometheusServer.GetMetricData("DiskWrite", NOW)
 
-	//for _, data := range DiskReadData {
-	//	println(data.NameSpace, data.Pod, data.Time.UnixMilli(), data.Value)
-	//}
+	// CPUData := PrometheusServer.GetMetricData("CPU", Before, Now)
+	MemoryData := PrometheusServer.GetMetricData("Memory", Before, Now)
+	//DiskReadData := PrometheusServer.GetMetricData("DiskRead", Before, Now)
+	//DiskWriteData := PrometheusServer.GetMetricData("DiskWrite", Before, Now)
+	//NetworkInData := PrometheusServer.GetMetricData("DiskWrite", Before, Now)
+	//NetworkOutData := PrometheusServer.GetMetricData("DiskWrite", Before, Now)
+
+	for _, data := range MemoryData {
+		for _, metricData := range data.Data {
+			println("NS", data.NameSpace, "Pod", data.Pod, "Container", data.Container, "TS", metricData.TimeStamp, "V", metricData.Value)
+			if data.Container == "" {
+				println("EMPTY C")
+			}
+		}
+	}
 }
