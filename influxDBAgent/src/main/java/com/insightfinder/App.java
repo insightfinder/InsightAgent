e323715560e6bf2f891dabf03a84d775f2486b92
elayrocks <kinghyl13@gmail.com>
2023-07-25 15:39:20 -0400

    init

--- influxDBAgent/src/main/java/com/insightfinder/App.java
+++ influxDBAgent/src/main/java/com/insightfinder/App.java
@@ -2,15 +2,20 @@ package com.insightfinder;
 
 import com.insightfinder.collector.InfluxDBMetricCollector;
 import com.insightfinder.datamodel.InstanceData;
-import com.insightfinder.datamodel.MetricDataPoint;
+import com.insightfinder.payload.MetricDataBody;
 import com.insightfinder.payload.MetricDataReceivePayload;
+import com.insightfinder.utilities.GsonUtility;
+import com.insightfinder.utilities.HttpUtility;
 import java.io.IOException;
+import java.net.http.HttpResponse;
 import java.util.Map;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class App {
+
+  public static final String INSIGHTFINDER_ENDPOINT = "http://localhost:8080/api/v2/metric-data-receive";
 
   private static final Logger logger = Logger.getLogger(App.class.getName());
 
@@ -28,16 +33,23 @@ public class App {
     String projectName = prop.getProperty("insightfinder.projectName");
     String userName = prop.getProperty("insightfinder.userName");
     String samplingInterval = prop.getProperty("insightfinder.samplingInterval");
+    String licenseKey = prop.getProperty("insightfinder.licenseKey");
 
     while (true) {
-      Thread.sleep(fetchInterval);
       long currentTimestamp = System.currentTimeMillis();
       logger.log(Level.INFO,
           "Start to collect data from " + lastFetchTimestamp + " to " + currentTimestamp);
       Map<String, InstanceData> dataMap = collector.collectData(0L, Long.MAX_VALUE);
-      MetricDataReceivePayload payload = new MetricDataReceivePayload(projectName, userName,
-          "influxDBAgent", samplingInterval, dataMap);
+      if (!dataMap.isEmpty()) {
+        MetricDataReceivePayload payload = new MetricDataReceivePayload(projectName, userName,
+            "influxDBAgent", samplingInterval, dataMap);
+        MetricDataBody metricDataBody = new MetricDataBody(payload, licenseKey, userName);
+        HttpResponse<String> response = HttpUtility.sendHttpRequest(INSIGHTFINDER_ENDPOINT,
+            GsonUtility.gson.toJson(metricDataBody));
+        logger.log(Level.INFO, "Response: " + response.body());
+      }
       lastFetchTimestamp = currentTimestamp;
+      Thread.sleep(fetchInterval);
     }
   }
 }
