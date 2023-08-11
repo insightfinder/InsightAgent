e72180e9ae9e5675e0a03ab94597254a36b292c6
maoyuwang <i@maoyu.wang>
2023-08-11 11:53:10 -0400

    Switch the logic to fetch data for pod instead of namespace.

--- kubernetes-agent/loki/utility.go
+++ kubernetes-agent/loki/utility.go
@@ -7,14 +7,14 @@ import (
 	"time"
 )
 
-func FormatQueryWithNamespaces(query string, namespaces string) string {
+func FormatQuery(query string, namespaces string, pod string) string {
 	namespaceList := strings.Split(namespaces, ",")
 	namespacePromStr := ""
 	for _, namespace := range namespaceList {
 		namespacePromStr += (namespace + "|")
 	}
 	namespacePromStr = namespacePromStr[:len(namespacePromStr)-1]
-	return fmt.Sprintf(query, namespacePromStr)
+	return fmt.Sprintf(query, namespacePromStr, pod)
 }
 
 func WithinTimeRange(startTime time.Time, endTime time.Time, duration time.Duration) bool {
