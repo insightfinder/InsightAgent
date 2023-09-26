0ad6db5d673f5fbb1bbce7f8c6d7fd899e48c7d0
maoyuwang <i@maoyu.wang>
2023-09-26 14:41:11 -0400

    Add logic to process node component names.

--- kubernetes-agent/tools/PostProcessor.go
+++ kubernetes-agent/tools/PostProcessor.go
@@ -6,18 +6,31 @@ import (
 )
 
 type PostProcessor struct {
+	target                  string
 	hideComponentNamePrefix string
+	nodeComponentName       string
 }
 
 func (processor *PostProcessor) Initialize(configFile *configparser.ConfigParser) {
 
+	// Initialize target
+	processor.target, _ = configFile.Get("general", "target")
+
+	// Initialize nodeComponentName
+	processor.nodeComponentName, _ = configFile.Get("general", "node_component_name")
+
 	// Initialize hideComponentNameRegx
 	processor.hideComponentNamePrefix, _ = configFile.Get("general", "hide_component_name_prefix")
+
 }
 
 func (processor *PostProcessor) ProcessComponentName(componentName string) string {
-	if processor.hideComponentNamePrefix != "" {
-		return strings.TrimPrefix(componentName, processor.hideComponentNamePrefix)
+	if processor.target == "node" {
+		return processor.nodeComponentName
+	} else {
+		if processor.hideComponentNamePrefix != "" {
+			return strings.TrimPrefix(componentName, processor.hideComponentNamePrefix)
+		}
 	}
 	return componentName
 }
