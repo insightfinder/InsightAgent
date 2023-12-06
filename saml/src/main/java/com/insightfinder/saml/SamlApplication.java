fee1b1541c5898021ba8dd63987aee70f9a29b78
Xiangkui <xguo@insightfinder.com>
2023-12-05 20:37:12 -0600

    add saml service

---
+++ saml/src/main/java/com/insightfinder/saml/SamlApplication.java
@@ -0,0 +1,12 @@
+package com.insightfinder.saml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamlApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SamlApplication.class, args);
+	}
+
+}
---
+++ saml/src/main/java/com/insightfinder/saml/controller/SamlV2Controller.java
@@ -0,0 +1,22 @@
+package com.insightfinder.saml.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrationRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController("/saml")
+public class SamlV2Controller {
+    @Autowired
+    private RelyingPartyRegistrationRepository relyingPartyRegistrationRepository;
+    @GetMapping("/registrations/{name}")
+    public String getAllRegistrations(@PathVariable String name){
+        if (name.equalsIgnoreCase("insightfinder")){
+            return "http://localhost:9900/saml2/authenticate/insightfinder";
+        }else {
+            return  "http://localhost:9900/saml2/authenticate/fairfaxcounty";
+        }
+    }
+}
