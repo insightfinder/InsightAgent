209ad714b86f1928aab81baaee6baf8d0fef61d0
Xiangkui <xguo@insightfinder.com>
2024-01-09 16:33:14 -0600

    update

--- kubeactions/src/main/java/com/insightfinder/kubeactions/controller/K8SController.java
+++ kubeactions/src/main/java/com/insightfinder/kubeactions/controller/K8SController.java
@@ -3,6 +3,7 @@ package com.insightfinder.kubeactions.controller;
 import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.models.V1Deployment;
 import io.kubernetes.client.openapi.models.V1Node;
+import io.kubernetes.client.openapi.models.V1Pod;
 import org.jose4j.json.internal.json_simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,46 +25,27 @@ public class K8SController {
     }
 
     @GetMapping("/pods")
-    public List<String> allPods(@RequestHeader(required = true) String serverId) throws ApiException {
+    public Map<String, List<String>> allPods(@RequestHeader(required = true) String serverId) throws ApiException {
         return k8SManager.getPods();
     }
 
-    @GetMapping("/nodes")
-    public Map<String, List<String>> nodeToPods(@RequestHeader(required = true) String serverId) throws ApiException {
-        return k8SManager.getNodes();
-    }
-
-    @GetMapping("/nodesinfo")
-    public List<V1Node> nodesinfo(@RequestHeader(required = true) String serverId) throws ApiException {
-        return k8SManager.listNodes();
-    }
-
-    @GetMapping("/nodes/{namespace}")
-    public Map<String, List<String>> nodeToPods(@RequestHeader(required = true) String serverId, @PathVariable String namespace) throws ApiException {
-        return k8SManager.getNodes(namespace);
-    }
-
-    @DeleteMapping("/del/{namespace}/{nodeName}/{podName}")
-    public ResponseEntity<String> deletePod(@RequestHeader(required = true) String serverId, @PathVariable String namespace, @PathVariable String nodeName, @PathVariable String podName) throws ApiException {
-        k8SManager.deletePod(nodeName, namespace, podName);
-        return new ResponseEntity<>(HttpStatus.OK);
+    @GetMapping("/pods/{namespace}/{podName}")
+    public JSONObject getPodInfo(@RequestHeader(required = true) String serverId, @PathVariable String namespace, @PathVariable String podName) throws ApiException {
+        StringBuilder stringBuilder = new StringBuilder();
+        V1Pod v1Pod = k8SManager.getPod(namespace, podName, stringBuilder);
+        JSONObject retValue = new JSONObject();
+       if (v1Pod != null){
+           retValue.put("pod", v1Pod.toString());
+       }else {
+           retValue.put("errors", stringBuilder.toString());
+       }
+       return retValue;
     }
 
     @PostMapping("/scale/{namespace}/{deployment}/{num}")
     public ResponseEntity<String> scaleBy(@RequestHeader(required = true) String serverId, @PathVariable String namespace, @PathVariable String deployment, @PathVariable int num) throws ApiException {
         k8SManager.scaleBy(namespace, deployment, num);
         return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @PostMapping("/scale/{namespace}/{deployment}/{cpu}/{mem}")
-    public ResponseEntity<String> verticalScaleBy(@RequestHeader(required = true) String serverId, @PathVariable String namespace, @PathVariable String deployment, @PathVariable float cpu, @PathVariable float mem) throws ApiException {
-        k8SManager.verticalScaleBy(namespace, deployment, cpu, mem);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @GetMapping("/limit/{namespace}/{deployment}/{container}")
-    public long getContainerMemLimit(@RequestHeader(required = true) String serverId, @PathVariable String namespace, @PathVariable String deployment, @PathVariable String container) throws ApiException {
-        return k8SManager.getContainerMemLimit(namespace, deployment, container);
     }
 
     @GetMapping("/res/{namespace}/{deployment}")
