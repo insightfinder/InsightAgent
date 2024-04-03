9fd5baeebab36a477edf22af0e46ee184584691f
Ashvat Bansal <ashvat@insightfinder.com>
2024-04-03 15:17:29 -0400

    updated entrypoint.sh for saml

--- saml/entrypoint.sh
+++ saml/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Create certificate and key for saml
-openssl req -newkey rsa:2048 -nodes -keyout sp.key -x509 -days 365 -out sp.crt -subj "/C=/ST=/L=/O=InsightFinder/OU=/CN=localhost"
+# clear cert sp directory
+rm -rf /app/certs/sp/*
 
-# Move cert and key to correct location
-mv sp.crt sp.key /app/certs/sp/
+# Create certificate and key for saml
+openssl req -newkey rsa:2048 -nodes -keyout /app/certs/sp/sp.key -x509 -days 365 -out /app/certs/sp/sp.crt -subj "/C=/ST=/L=/O=InsightFinder/OU=/CN=localhost"
 
 # Run the application
 java -jar app.jar --spring.config.location=file:/app/config/application.yml
