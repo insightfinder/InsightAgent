a30511def07389b3970d663dbf23149d582ef7a9
joeyw <wangjq199798@sina.com>
2024-06-11 13:01:06 -0400

    retry on close

--- kubeactions/src/main/java/com/insightfinder/kubeactions/client/websocket/WebSocketSessionHandler.java
+++ kubeactions/src/main/java/com/insightfinder/kubeactions/client/websocket/WebSocketSessionHandler.java
@@ -2,7 +2,9 @@ package com.insightfinder.kubeactions.client.websocket;
 
 import com.insightfinder.InsightFinderAgent.proto.IfAgentInstructionWrapper.IfAgentInstruction;
 import com.insightfinder.kubeactions.client.websocket.handler.K8sInstructionHandler;
-import com.insightfinder.kubeactions.client.websocket.handler.WebSocketConnectionCloseHandler;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,16 +15,25 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.client.WebSocketConnectionManager;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 
 @Component
 public class WebSocketSessionHandler implements WebSocketHandler {
 
   private static final Logger log = LoggerFactory.getLogger(WebSocketSessionHandler.class);
+  private static final int CONNECTION_RETRY_INTERVAL = 5;
 
   @Autowired
   private K8sInstructionHandler k8sInstructionHandler;
+  private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
   @Autowired
-  private WebSocketConnectionCloseHandler webSocketConnectionCloseHandler;
+  private WebSocketUtils webSocketUtils;
+  private WebSocketConnectionManager connectionManager;
+
+  public void setConnectionManager(WebSocketConnectionManager connectionManager) {
+    this.connectionManager = connectionManager;
+  }
 
   /**
    * Called when WS connects to the server.
@@ -38,12 +49,11 @@ public class WebSocketSessionHandler implements WebSocketHandler {
   @Override
   public void handleMessage(WebSocketSession session, WebSocketMessage<?> message)
       throws Exception {
-    if (message instanceof TextMessage textMessage) {
-      System.out.println(textMessage.getPayload());
-    } else if (message instanceof BinaryMessage binaryMessage) {
+    if (message instanceof BinaryMessage binaryMessage) {
       IfAgentInstruction ifAgentInstruction = IfAgentInstruction.parseFrom(
           binaryMessage.getPayload());
       k8sInstructionHandler.handlerK8sInstruction(ifAgentInstruction);
+    } else if (message instanceof TextMessage textMessage) {
     } else {
       log.warn("Unable to handle message: " + message.getClass());
     }
@@ -55,6 +65,9 @@ public class WebSocketSessionHandler implements WebSocketHandler {
   @Override
   public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
     log.error("WebSocket transport error: " + exception.getMessage());
+    if (!connectionManager.isConnected()) {
+      scheduleReconnection();
+    }
   }
 
   /**
@@ -64,6 +77,7 @@ public class WebSocketSessionHandler implements WebSocketHandler {
   public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus)
       throws Exception {
     log.info("WebSocket connection closed, closeStatus: " + closeStatus.getReason());
+    scheduleReconnection();
   }
 
   @Override
@@ -72,4 +86,21 @@ public class WebSocketSessionHandler implements WebSocketHandler {
     return false;
   }
 
+  private void scheduleReconnection() {
+    scheduler.scheduleAtFixedRate(() -> {
+      if (!connectionManager.isConnected()) {
+        try {
+          connectionManager.stop();
+          String webSocketUrl = webSocketUtils.getWebsocketServerUrl();
+          connectionManager = new WebSocketConnectionManager(
+              new StandardWebSocketClient(),
+              this,
+              webSocketUrl);
+          connectionManager.start();
+        } catch (Exception e) {
+          log.warn("Error when reconnecting webSocket: " + e.getMessage());
+        }
+      }
+    }, CONNECTION_RETRY_INTERVAL, CONNECTION_RETRY_INTERVAL, TimeUnit.SECONDS);
+  }
 }
--- kubeactions/src/main/java/com/insightfinder/kubeactions/config/WebSocketConfig.java
+++ kubeactions/src/main/java/com/insightfinder/kubeactions/config/WebSocketConfig.java
@@ -1,47 +1,27 @@
 package com.insightfinder.kubeactions.config;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import com.insightfinder.kubeactions.client.websocket.WebSocketSessionHandler;
-import org.jetbrains.annotations.NotNull;
-import org.mapdb.DB;
+import com.insightfinder.kubeactions.client.websocket.WebSocketUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.socket.WebSocketHttpHeaders;
 import org.springframework.web.socket.client.WebSocketConnectionManager;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 
 @Configuration
 public class WebSocketConfig {
-
   private static final Logger log = LoggerFactory.getLogger(WebSocketConfig.class);
-  private static final String WEB_SOCKET_URL_PARAM = "wsUrl";
   private static final String CLIENT_ID_HEADER = "Client-Id";
-  private static final int RECONNECT_DELAY = 5000;
   @Autowired
-  private IFConfig ifConfig;
-  @Autowired
-  private DB mapDB;
-  @Autowired
-  private Gson gson;
-  @Autowired
-  private RestTemplate restTemplate;
-  @Autowired
-  private WebSocketSessionHandler webSocketSessionHandler;
+  IFConfig ifConfig;
 
   @Bean
-  public WebSocketConnectionManager webSocketConnectionManager() {
-    String webSocketUrl = getWebsocketServerUrl();
+  public WebSocketConnectionManager webSocketConnectionManager(
+      WebSocketSessionHandler webSocketSessionHandler, WebSocketUtils webSocketUtils) {
+    String webSocketUrl = webSocketUtils.getWebsocketServerUrl();
     if (webSocketUrl == null) {
       log.warn("Couldn't establish webSocket connection: fail to get webSocket server URL");
       return null;
@@ -57,31 +37,9 @@ public class WebSocketConfig {
     //Will connect as soon as possible
     manager.setAutoStartup(true);
     manager.setHeaders(headers);
+    webSocketSessionHandler.setConnectionManager(manager);
     return manager;
   }
 
-  private String getWebsocketServerUrl() {
-    String url = String.format("http://localhost:8080/api/v1/wss-agent-ops",
-        ifConfig.getServerUrl());
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-    HttpEntity<MultiValueMap<String, String>> request = getMultiValueMapHttpEntity(headers);
-    ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
-    if (response.getStatusCode().is2xxSuccessful()) {
-      JsonObject resObj = gson.fromJson(response.getBody(), JsonObject.class);
-      return resObj.get(WEB_SOCKET_URL_PARAM).getAsString();
-    }
-    return null;
-  }
 
-  @NotNull
-  private HttpEntity<MultiValueMap<String, String>> getMultiValueMapHttpEntity(
-      HttpHeaders headers) {
-    MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-    map.add("userName", ifConfig.getUserName());
-    map.add("systemName", ifConfig.getSystem());
-    map.add("serverId", ifConfig.getActionServerId());
-    map.add("ops", "startWss");
-    return new HttpEntity<>(map, headers);
-  }
 }
