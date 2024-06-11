f6f801581fe44fb1f8dcc718e970952a51724ed4
joeyw <wangjq199798@sina.com>
2024-06-11 18:11:09 -0400

    update

--- kubeactions/src/main/java/com/insightfinder/kubeactions/config/WebSocketConfig.java
+++ kubeactions/src/main/java/com/insightfinder/kubeactions/config/WebSocketConfig.java
@@ -5,6 +5,7 @@ import com.insightfinder.kubeactions.client.websocket.WebSocketUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHttpHeaders;
@@ -13,23 +14,22 @@ import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 
 @Configuration
 public class WebSocketConfig {
+
   private static final Logger log = LoggerFactory.getLogger(WebSocketConfig.class);
   private static final String CLIENT_ID_HEADER = "Client-Id";
   @Autowired
   IFConfig ifConfig;
 
   @Bean
+  @Qualifier("webSocketConnectionManager")
   public WebSocketConnectionManager webSocketConnectionManager(
       WebSocketSessionHandler webSocketSessionHandler, WebSocketUtils webSocketUtils) {
+    WebSocketConnectionManager manager;
     String webSocketUrl = webSocketUtils.getWebsocketServerUrl();
-    if (webSocketUrl == null) {
-      log.warn("Couldn't establish webSocket connection: fail to get webSocket server URL");
-      return null;
-    }
     WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
     headers.add(CLIENT_ID_HEADER, ifConfig.getActionServerId());
     //Generates a web socket connection
-    WebSocketConnectionManager manager = new WebSocketConnectionManager(
+    manager = new WebSocketConnectionManager(
         new StandardWebSocketClient(),
         webSocketSessionHandler,
         webSocketUrl);
@@ -38,8 +38,7 @@ public class WebSocketConfig {
     manager.setAutoStartup(true);
     manager.setHeaders(headers);
     webSocketSessionHandler.setConnectionManager(manager);
+    manager.start();
     return manager;
   }
-
-
 }
