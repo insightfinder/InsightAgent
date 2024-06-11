038b0a24e8d7b1bf8c526278d7b881123c89f131
joeyw <wangjq199798@sina.com>
2024-06-11 18:11:44 -0400

    update

--- kubeactions/src/main/java/com/insightfinder/kubeactions/KubeActionsApplication.java
+++ kubeactions/src/main/java/com/insightfinder/kubeactions/KubeActionsApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KubeactionsApplication implements CommandLineRunner {
+public class KubeActionsApplication implements CommandLineRunner {
 
   public static void main(String[] args) {
-    SpringApplication.run(KubeactionsApplication.class, args);
+    SpringApplication.run(KubeActionsApplication.class, args);
   }
 
   @Override
