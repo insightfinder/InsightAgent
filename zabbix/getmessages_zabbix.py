828d6f98a211746324c064e33e1570310a4aa598
Maoyu <i@maoyu.wang>
2025-03-11 17:46:46 -0400

    Fix zoneName in log data payload

--- zabbix/getmessages_zabbix.py
+++ zabbix/getmessages_zabbix.py
@@ -504,7 +504,7 @@ def parse_messages_zabbix(logger, data_type, result, all_field_map, items_map, r
                 data_buffer['buffer_dict'][key]['tag'] = full_instance
                 if component:
                     data_buffer['buffer_dict'][key]['componentName'] = component
-                    data_buffer['buffer_dict'][key]['zone'] = zone
+                    data_buffer['buffer_dict'][key]['zoneName'] = zone
                 data_buffer['buffer_dict'][key]['data'] = data_value
 
         except Exception as e:
