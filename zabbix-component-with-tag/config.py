bc2bbf828ca503f1a19823a70c57f9efd380cb9d
Ashvat <ashvat@insightfinder.com>
2025-08-01 17:55:15 -0400

    removed sensitive information

--- zabbix-component-with-tag/config.py
+++ zabbix-component-with-tag/config.py
@@ -1,12 +1,12 @@
 # Configurations for Zabbix
-zabbix_url = "http://localhost:9999/zabbix"
-zabbix_user = "insight.finder"
-zabbix_password = "2ri2T*%HKxbs"
+zabbix_url = ""
+zabbix_user = ""
+zabbix_password = ""
 
 # Configurations for InsightFinder
-insightfinder_url = "https://stg.insightfinder.com"
-insightfinder_username = "ashvat"
-insightfinder_password = "SynysterAshi@01"
-license_key = "fa28bf0a1a0f6e77c6eea446383b36d0b5a884d2"
-insightfinder_project = "zabbix-metadata-4"
+insightfinder_url = "https://app.insightfinder.com"
+insightfinder_username = ""
+insightfinder_password = ""
+license_key = ""
+insightfinder_project = ""
 user_agent = "Mozilla/5.0 (compatible; InsightFinderClient/1.0;)"
