d5efcf080e171d8637a53e09efcdf24e2ce2bfc9
Mustafa M <neyaadeez@gmail.com>
2025-08-06 14:08:07 -0400

    [II-20144] AccessParks - Add RSSI metrics to Cambium agent

--- cambium-agent/models/models.go
+++ cambium-agent/models/models.go
@@ -18,6 +18,8 @@ type Device struct {
 	Net    NetworkInfo `json:"net"`
 	Config ConfigInfo  `json:"config"`
 	Cfg    CfgInfo     `json:"cfg"`
+	Mode   string      `json:"mode"`
+	Nid    string      `json:"nid"`
 }
 
 type SystemInfo struct {
@@ -55,63 +57,64 @@ type DevicesResponse struct {
 // Radio represents radio data from the stats API
 type Radio struct {
 	// Common fields
-	Band    string      `json:"band"`
-	Channel interface{} `json:"channel"` // Can be string or int
-	DlTPut  float64     `json:"dlTPut"`
-	UlTPut  float64     `json:"ulTPut"`
-	Mus     int         `json:"mus"`
-	Nf      float64     `json:"nf"`
-	TotalCu float64     `json:"total_cu"`
+	Band    string  `json:"band"`
+	Mus     int     `json:"mus"`
+	TotalCu float64 `json:"total_cu"`
+
+	// Channel interface{} `json:"channel"` // Can be string or int
+	// DlTPut  float64     `json:"dlTPut"`
+	// UlTPut  float64     `json:"ulTPut"`
+	// Nf      float64     `json:"nf"`
 
 	// Additional fields for different radio types
-	Id        interface{} `json:"id"` // Can be string or int
-	Mac       string      `json:"mac"`
-	Pmac      string      `json:"pmac"`
-	Cid       string      `json:"cid"`
-	State     string      `json:"state"`
-	ChWidth   string      `json:"chWidth"`
-	ChanIf    int         `json:"chan_if"`
-	ChannelUt int         `json:"channel_ut"`
-	MMus      int         `json:"mMus"`
-	McRate    string      `json:"mcRate"`
-	MeshState string      `json:"meshState"`
-	Nav       int         `json:"nav"`
-	Per       int         `json:"per"`
-	Pow       int         `json:"pow"`
-	RfUtil    int         `json:"rfUtil"`
-	Rfqlt     int         `json:"rfqlt"`
-	Rx        float64     `json:"rx"`
-	RxAvg     int         `json:"rxAvg"`
-	RxMax     int         `json:"rxMax"`
-	RxMin     int         `json:"rxMin"`
-	RxPkts    int64       `json:"rxPkts"`
-	RxCu      int         `json:"rx_cu"`
-	Ts        int64       `json:"ts"`
-	Tx        float64     `json:"tx"`
-	TxAvg     int         `json:"txAvg"`
-	TxMax     int         `json:"txMax"`
-	TxMin     int         `json:"txMin"`
-	TxPkts    int64       `json:"txPkts"`
-	TxCu      int         `json:"tx_cu"`
-	UcRates   string      `json:"ucRates"`
-	Wlans     int         `json:"wlans"`
-	BusyCu    int         `json:"busy_cu"`
+	// Id        interface{} `json:"id"` // Can be string or int
+	// Mac       string      `json:"mac"`
+	// Pmac      string      `json:"pmac"`
+	// Cid       string      `json:"cid"`
+	// State     string      `json:"state"`
+	// ChWidth   string      `json:"chWidth"`
+	// ChanIf    int         `json:"chan_if"`
+	// ChannelUt int         `json:"channel_ut"`
+	// MMus      int         `json:"mMus"`
+	// McRate    string      `json:"mcRate"`
+	// MeshState string      `json:"meshState"`
+	// Nav       int         `json:"nav"`
+	// Per       int         `json:"per"`
+	// Pow       int         `json:"pow"`
+	// RfUtil    int         `json:"rfUtil"`
+	// Rfqlt     int         `json:"rfqlt"`
+	// Rx        float64     `json:"rx"`
+	// RxAvg     int         `json:"rxAvg"`
+	// RxMax     int         `json:"rxMax"`
+	// RxMin     int         `json:"rxMin"`
+	// RxPkts    int64       `json:"rxPkts"`
+	// RxCu      int         `json:"rx_cu"`
+	// Ts        int64       `json:"ts"`
+	// Tx        float64     `json:"tx"`
+	// TxAvg     int         `json:"txAvg"`
+	// TxMax     int         `json:"txMax"`
+	// TxMin     int         `json:"txMin"`
+	// TxPkts    int64       `json:"txPkts"`
+	// TxCu      int         `json:"tx_cu"`
+	// UcRates   string      `json:"ucRates"`
+	// Wlans     int         `json:"wlans"`
+	// BusyCu    int         `json:"busy_cu"`
 
-	// Fields for other radio configurations
-	BaseBandTemp           int    `json:"baseBandTemp,omitempty"`
-	ConfiguredTddSlotRatio string `json:"configuredTddSlotRatio,omitempty"`
-	CurrentTddSlotRatio    string `json:"currentTddSlotRatio,omitempty"`
-	ErrAssoc               int    `json:"errAssoc,omitempty"`
-	NumSwitches            int    `json:"numSwitches,omitempty"`
-	Polarity               int    `json:"polarity,omitempty"`
-	RfTile0Temp            int    `json:"rfTile0Temp,omitempty"`
-	RfTile1Temp            int    `json:"rfTile1Temp,omitempty"`
-	RfTile2Temp            int    `json:"rfTile2Temp,omitempty"`
-	RfTile3Temp            int    `json:"rfTile3Temp,omitempty"`
-	Security               int    `json:"security,omitempty"`
-	SetErrChannel          int    `json:"setErrChannel,omitempty"`
-	SyncModeGps            int    `json:"syncModeGps,omitempty"`
-	SyncModeRf             int    `json:"syncModeRf,omitempty"`
+	// // Fields for other radio configurations
+	// BaseBandTemp           int    `json:"baseBandTemp,omitempty"`
+	// ConfiguredTddSlotRatio string `json:"configuredTddSlotRatio,omitempty"`
+	// CurrentTddSlotRatio    string `json:"currentTddSlotRatio,omitempty"`
+	// ErrAssoc               int    `json:"errAssoc,omitempty"`
+	// NumSwitches            int    `json:"numSwitches,omitempty"`
+	// Polarity               int    `json:"polarity,omitempty"`
+	// RfTile0Temp            int    `json:"rfTile0Temp,omitempty"`
+	// RfTile1Temp            int    `json:"rfTile1Temp,omitempty"`
+	// RfTile2Temp            int    `json:"rfTile2Temp,omitempty"`
+	// RfTile3Temp            int    `json:"rfTile3Temp,omitempty"`
+	// Security               int    `json:"security,omitempty"`
+	// SetErrChannel          int    `json:"setErrChannel,omitempty"`
+	// SyncModeGps            int    `json:"syncModeGps,omitempty"`
+	// SyncModeRf             int    `json:"syncModeRf,omitempty"`
 }
 
 // RadiosResponse represents the API response for radio stats
@@ -123,6 +126,38 @@ type RadiosResponse struct {
 			Total  int `json:"total"`
 		} `json:"_metadata"`
 		Radios []Radio `json:"radios"`
+	} `json:"data"`
+	STime int64 `json:"sTime"`
+}
+
+// Link represents a single link in the E2E data
+type Link struct {
+	RSSI int `json:"rssi"`
+	SNR  int `json:"snr"`
+
+	// ZMAC          string `json:"zmac"`
+	// AMAC          string `json:"amac"`
+	// EIRP          int    `json:"eirp"`
+	// LinkAvailable int    `json:"linkAvailable"`
+	// Name          string `json:"name"`
+	// RxMCS         int    `json:"rxMcs"`
+	// TS            int64  `json:"ts"`
+	// TxMCS         int    `json:"txMcs"`
+	// Direction     int    `json:"direction"`
+	// ID            string `json:"_id"`
+	// ANode         string `json:"aNode"`
+	// ZNode         string `json:"zNode"`
+}
+
+// E2EData represents the API response for E2E link data
+type E2EData struct {
+	Data struct {
+		Metadata struct {
+			TotalCount int `json:"totalCount"`
+			Limit      int `json:"limit"`
+			Offset     int `json:"offset"`
+		} `json:"_metadata"`
+		Links []Link `json:"links"`
 	} `json:"data"`
 	STime int64 `json:"sTime"`
 }
