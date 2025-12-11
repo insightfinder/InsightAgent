2d31e4405ca2eecb60e356e6e0817ebbf5af7fcf
Ashvat <ashvat@insightfinder.com>
2025-12-11 15:58:49 -0500

    init zabbix-mas

---
+++ zabbix-maps/config.py
@@ -0,0 +1,13 @@
+# Configurations for Zabbix
+zabbix_url = "http://localhost:9999/zabbix"
+zabbix_user = "insight.finder"
+zabbix_password = "2ri2T*%HKxbs"
+
+# Configurations for InsightFinder
+insightfinder_url = "https://stg.insightfinder.com"
+insightfinder_username = "ashvat"
+insightfinder_password = "SynysterAshi@01"
+license_key = "fa28bf0a1a0f6e77c6eea446383b36d0b5a884d2"
+insightfinder_project = "zabbix-map-test"
+insightfinder_system = "Ashvat-Test"  # System display name (can be different from project)
+user_agent = "Mozilla/5.0 (compatible; InsightFinderClient/1.0;)"
